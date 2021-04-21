94be1dd8242126d2a34f1f3ee06bd311e23aea67
Noam Almog <noama@wix.com>
2021-04-21 15:53:25 +0300

    data stub

--- serverless/wordpress-external-db/serverless.js
+++ serverless/wordpress-external-db/serverless.js
@@ -37,9 +37,22 @@ class WordPressServiceImpl extends com.wixpress.wordpress.WordPressService {
     }
 }
 
-// // *************** INFO **********************
-//
-// // *************** Data API **********************
+module.exports = fb => fb.addGrpcService(WordPressServiceImpl)
+                         .addWebFunction('POST', '/provision', async () => { return {} })
+                         .addWebFunction('POST', '/schemas/list', async () => { return { schemas: dbs } })
+                         .addWebFunction('POST', '/schemas/find', async (ctx, req) => {
+                             const { schemaIds } = req.body
+                             return { schemas: dbs.filter(item => schemaIds.includes( item.id )) } })
+
+                         .addWebFunction('POST', '/data/find', async (ctx, req) => { return { items: [], totalCount: 0 } })
+                         .addWebFunction('POST', '/data/count', async (ctx, req) => { return { totalCount: 0 } })
+                         .addWebFunction('POST', '/data/get', async (ctx, req) => { return { item: { } } })
+                         .addWebFunction('POST', '/data/insert', async (ctx, req) => { return { item: { } } })
+                         .addWebFunction('POST', '/data/update', async (ctx, req) => { return { item: { } } })
+                         .addWebFunction('POST', '/data/remove', async (ctx, req) => { return { item: { } } })
+
+
+
 // app.post('/data/find', async (req, res) => {
 //     const { collectionName, filter, sort, skip, limit } = req.body
 //     const data = await dataService.find(collectionName, filter, sort, skip, limit)
@@ -76,48 +89,6 @@ class WordPressServiceImpl extends com.wixpress.wordpress.WordPressService {
 //     res.json(data)
 // })
 // // ***********************************************
-//
-//
-
-
-module.exports = fb => fb.addGrpcService(WordPressServiceImpl)
-                         .addWebFunction('POST', '/provision', async () => { return {} })
-                         .addWebFunction('POST', '/schemas/list', async () => { return { schemas: dbs } })
-                         .addWebFunction('POST', '/schemas/find', async (ctx, req) => {
-                             const { schemaIds } = req.body
-                             return { schemas: dbs.filter(item => schemaIds.includes( item.id )) } })
 
 
 
-
-
-// // *************** Schema API **********************
-// app.post('/schemas/list', async (req, res) => {
-//     const data = await schemaService.list()
-//     res.json(data)
-// })
-//
-// app.post('/schemas/find', async (req, res) => {
-//     const { schemaIds } = req.body
-//     const data = await schemaService.find(schemaIds)
-//     res.json(data)
-// })
-//
-// app.post('/schemas/create', async (req, res) => {
-//     const { collectionName } = req.body
-//     const data = await schemaService.create(collectionName)
-//     res.json(data)
-// })
-//
-// app.post('/schemas/column/add', async (req, res) => {
-//     const { collectionName, column } = req.body
-//     const data = await schemaService.addColumn(collectionName, column)
-//     res.json(data)
-// })
-//
-// app.post('/schemas/column/remove', async (req, res) => {
-//     const { collectionName, columnName } = req.body
-//     const data = await schemaService.removeColumn(collectionName, columnName)
-//     res.json(data)
-// })
-// // ***********************************************
