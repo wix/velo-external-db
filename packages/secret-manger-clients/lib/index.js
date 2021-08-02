48ba99c0369112cb5624f00421f30cc388c4f50d
Max Polsky <maxim.polski@gmail.com>
2021-08-02 13:10:54 +0300

    init of secret manager client package

---
+++ packages/secret-manger-clients/lib/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {SecretMangerClientENV,SecretMangerClientAWS, SecretMangerClientAzure, SecretMangerClientGCP } = require('./secretMagerClients')
+
+const createSecretClient = (type) => { 
+    switch ( type ) {
+        case 'env/sql':
+            console.log(`SECRET MANGER: ${type}`);
+            return new SecretMangerClientENV();
+        case 'aws/sql':
+            console.log(`SECRET MANGER: ${type}`);
+            return  new SecretMangerClientAWS();   
+        case 'azr/sql':
+            console.log(`SECRET MANGER: ${type}`);
+            return new SecretMangerClientAzure(); 
+        case 'gcp/sql':
+            console.log(`SECRET MANGER: ${type}`);
+            return new SecretMangerClientGCP(); 
+        default:
+            throw new Error (`Type variable not supplied or not recognized.`);
+    }
+}
+
+module.exports = { createSecretClient };
+
