b4ba1a846ece0fdc8d77f036034ea02dfa43f840
Noam Almog <noama@wix.com>
2021-08-02 16:25:57 +0300

    removed dead code

--- packages/external-db-mysql/lib/mysql_utils.js
+++ packages/external-db-mysql/lib/mysql_utils.js
@@ -1,5 +1,3 @@
 const wildCardWith = (n, char) => Array(n).fill(char, 0, n).join(', ')
 
-const isObject = (o) => typeof o === 'object' && o !== null
-
-module.exports = { wildCardWith, isObject }
+module.exports = { wildCardWith }
