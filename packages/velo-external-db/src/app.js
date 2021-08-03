0db2826661d6ce07521a5fd9a946af7365879cc7
Ido <idokah@wix.com>
2021-08-03 14:42:51 +0300

    Revert "create router file and load async (#5)"
    This reverts commit fff176d0b9a765070f0c317f4c9999e83d9e04f3.

--- packages/velo-external-db/src/app.js
+++ packages/velo-external-db/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const { DataService, SchemaService } = require('velo-external-db-core')
@@ -6,46 +7,183 @@ const { init } = require('./storage/factory')
 const { errorMiddleware } = require('./web/error-middleware')
 const { authMiddleware } = require('./web/auth-middleware')
 const { unless } = require('./web/middleware-support')
-const createRouter = require('./router')
-const path = require('path')
 
-const load = async () => {
-    const { type, host, user, password, db, cloudSqlConnectionName } = { type: process.env.TYPE, host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD, db: process.env.DB, cloudSqlConnectionName: process.env.CLOUD_SQL_CONNECTION_NAME }
+let dataService, schemaService, cleanup
 
-    const { dataProvider, schemaProvider, cleanup: cleanupDbFunc, databaseOperations } = init(type, host, user, password, db, cloudSqlConnectionName)
-    await databaseOperations.checkIfConnectionSucceeded()
-    const dataService = new DataService(dataProvider)
-    const schemaService = new SchemaService(schemaProvider)
-    const cleanup = cleanupDbFunc
+const load = () => {
+    const { type, host, user, password, db, cloudSqlConnectionName } = {type: process.env.TYPE, host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD, db: process.env.DB, cloudSqlConnectionName: process.env.CLOUD_SQL_CONNECTION_NAME}
 
-    return { dataService, schemaService, cleanup }
+    const {dataProvider, schemaProvider, cleanup: cleanupDbFunc} = init(type, host, user, password, db, cloudSqlConnectionName)
+    dataService = new DataService(dataProvider)
+    schemaService = new SchemaService(schemaProvider)
+    cleanup = cleanupDbFunc
 }
 
-load().then(async (res) => {
-    const app = express()
-    const port = process.env.PORT || 8080
+load()
 
-    app.use('/assets', express.static(path.join(__dirname, '..', 'assets')))
-    app.use(bodyParser.json())
-    app.use(unless(['/', '/provision'], authMiddleware({ secretKey: process.env.SECRET_KEY })));
-    app.use(errorMiddleware)
-    app.use(compression())
+const app = express()
+const port = process.env.PORT || 8080
 
-    const router = await createRouter(res.dataService, res.schemaService)
-    app.use('/', router)
+app.use(bodyParser.json())
+app.use(unless(['/', '/provision'], authMiddleware({ secretKey: process.env.SECRET_KEY })));
+app.use(errorMiddleware)
+app.use(compression())
+app.use('/assets', express.static(path.join(__dirname, '..', 'assets')))
 
-    const server = app.listen(port)
-
-    module.exports = { server, cleanup: res.cleanup, load };
-}).catch(err => {
-    const app = express()
-    const port = process.env.PORT || 8080
-    app.use('/', (req, res) => {
-        res.send(err.message)
-    })
-
-    const server = app.listen(port)
-
-    module.exports = { server, cleanup: undefined, load };
+// *************** INFO **********************
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
 })
 
+app.post('/provision', (req, res) => {
+    res.json({});
+})
+
+
+// *************** Data API **********************
+app.post('/data/find', async (req, res, next) => {
+    try {
+        const { collectionName, filter, sort, skip, limit } = req.body
+        const data = await dataService.find(collectionName, filter, sort, skip, limit)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/aggregate', async (req, res, next) => {
+    try {
+        const { collectionName, filter, aggregation } = req.body
+        const data = await dataService.aggregate(collectionName, filter, aggregation)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/insert', async (req, res, next) => {
+    try {
+        const { collectionName, item } = req.body
+        const data = await dataService.insert(collectionName, item)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/insert/bulk', async (req, res, next) => {
+    try {
+        const { collectionName, items } = req.body
+        const data = await dataService.bulkInsert(collectionName, items)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/get', async (req, res, next) => {
+    try {
+        const { collectionName, itemId } = req.body
+        const data = await dataService.getById(collectionName, itemId)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/update', async (req, res, next) => {
+    try {
+        const { collectionName, item } = req.body
+        const data = await dataService.update(collectionName, item)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/update/bulk', async (req, res, next) => {
+    try {
+        const { collectionName, items } = req.body
+        const data = await dataService.bulkUpdate(collectionName, items)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/remove', async (req, res, next) => {
+    try {
+        const { collectionName, itemId } = req.body
+        const data = await dataService.delete(collectionName, itemId)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/remove/bulk', async (req, res, next) => {
+    try {
+        const { collectionName, itemIds } = req.body
+        const data = await dataService.bulkDelete(collectionName, itemIds)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/count', async (req, res, next) => {
+    try {
+        const { collectionName, filter } = req.body
+        const data = await dataService.count(collectionName, filter)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/data/truncate', async (req, res, next) => {
+    try {
+        const { collectionName } = req.body
+        const data = await dataService.truncate(collectionName)
+        res.json(data)
+    } catch (e) {
+        next(e)
+    }
+})
+// ***********************************************
+
+
+// *************** Schema API **********************
+app.post('/schemas/list', async (req, res) => {
+    const data = await schemaService.list()
+    res.json(data)
+})
+
+app.post('/schemas/find', async (req, res) => {
+    const { schemaIds } = req.body
+    const data = await schemaService.find(schemaIds)
+    res.json(data)
+})
+
+app.post('/schemas/create', async (req, res) => {
+    const { collectionName } = req.body
+    const data = await schemaService.create(collectionName)
+    res.json(data)
+})
+
+app.post('/schemas/column/add', async (req, res) => {
+    const { collectionName, column } = req.body
+    const data = await schemaService.addColumn(collectionName, column)
+    res.json(data)
+})
+
+app.post('/schemas/column/remove', async (req, res) => {
+    const { collectionName, columnName } = req.body
+    const data = await schemaService.removeColumn(collectionName, columnName)
+    res.json(data)
+})
+// ***********************************************
+
+const server = app.listen(port)
+
+module.exports = { server, cleanup, load};
