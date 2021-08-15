3f800410f861d8fa25aa63b491f56c5aaf14b6cd
Max Polsky <maxim.polski@gmail.com>
2021-08-15 15:26:25 +0300

    removed reduntat console log prints

--- packages/external-db-config/lib/index.js
+++ packages/external-db-config/lib/index.js
@@ -4,19 +4,15 @@ const createExternalDbConfigClient = (type) => {
     switch (type) {
         case 'sql/mysql':
         case 'sql/postgres':
-            console.log(`EXTERNAL DB CONFIG: ${type}`);
             return new ExternalDbConfigClient();
         case 'aws/mysql':
         case 'aws/postgres':
-            console.log(`EXTERNAL DB CONFIG: ${type}`);
             return new ExternalDbConfigClientAWS();
         case 'azr/mysql':
         case 'azr/postgres':
-            console.log(`EXTERNAL DB CONFIG: ${type}`);
             return new ExternalDbConfigClient();
         case 'gcp/mysql':
         case 'gcp/postgres':
-            console.log(`EXTERNAL DB CONFIG: ${type}`);
             return new ExternalDbConfigClientGCP();
         default:
             throw new Error(`Type variable not supplied or not recognized.`);
