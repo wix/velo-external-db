7e9009c7d6423d5e46fc1f92f8b1643d44426b40
Max Polsky <maxim.polski@gmail.com>
2021-08-19 18:43:55 +0300

    external db config refactor to support secret providers

--- packages/external-db-config/lib/client_provider.js
+++ packages/external-db-config/lib/client_provider.js
@@ -1,19 +1,24 @@
-const { ExternalDbConfigClient, ExternalDbConfigClientAWS, ExternalDbConfigClientGCP } = require('./external_db_config_clients')
+const { ExternalDbConfigClient } = require('./external_db_config_clients')
+const { AwsSecretsProvider } = require('./service/aws_secrets_provider')
+const { GcpSecretsProvider } = require('./service/gcp_secrets_provider')
+const { SecretsProvider } = require('./service/secrets_provider')
 
 const createExternalDbConfigClient = (vendor) => {
-    // create impl from vendor
-    // read config
-    switch (vendor) {
-        case 'azr':
-            return new ExternalDbConfigClient();
-        case 'aws':
-            return new ExternalDbConfigClientAWS();
-        case 'gcp':
-            return new ExternalDbConfigClientGCP();
-        default:
-            throw new Error(`Type variable not supplied or not recognized [${vendor}].`);
-    }
+  // create impl from vendor
+  // read config
+  switch (vendor) {
+    case 'aws':
+      const awsSecretProvider = new AwsSecretsProvider()
+      return new ExternalDbConfigClient(awsSecretProvider)
+    case 'gcp':
+      const gcpSecretProvider = new GcpSecretsProvider()
+      return new ExternalDbConfigClient(gcpSecretProvider)
+    case 'azr':
+      const azrSecretsProvider = new SecretsProvider()
+      return new ExternalDbConfigClient(azrSecretsProvider)
+    default:
+      return new ExternalDbConfigClient()
+  }
 }
 
-module.exports = { createExternalDbConfigClient };
-
+module.exports = { createExternalDbConfigClient }
