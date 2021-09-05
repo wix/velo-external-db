bac2b3e3b75f17ec328b5745984e493d77862809
Noam Almog <noama@wix.com>
2021-09-05 17:26:16 +0300

    debugging mysql build on github

--- packages/velo-external-db/test/resources/engines/docker_support.js
+++ packages/velo-external-db/test/resources/engines/docker_support.js
@@ -1,6 +1,6 @@
 const compose = require('docker-compose')
 
-const runImage = async (image) => await compose.upOne(image, { cwd: __dirname, log: true })
+const runImage = async (image) => await compose.upOne(image, { cwd: __dirname, log: true, commandOptions: [['--force-recreate', '--remove-orphans']] })
 const stopImage = async (image) => await compose.stopOne(image, { cwd: __dirname, log: true })
 
 module.exports = { runImage, stopImage }
