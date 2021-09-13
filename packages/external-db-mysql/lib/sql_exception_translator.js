0d972f8ef692ccc0cd756cd3c0a66c7e7da3622d
Noam Almog <noama@wix.com>
2021-09-13 16:40:16 +0300

    fixed error

--- packages/external-db-mysql/lib/sql_exception_translator.js
+++ packages/external-db-mysql/lib/sql_exception_translator.js
@@ -8,12 +8,12 @@ const notThrowingTranslateErrorCodes = err => {
             return new FieldAlreadyExists('Collection already has a field with the same name')
         case 'ER_NO_SUCH_TABLE':
             return new CollectionDoesNotExists('Collection does not exists')
-        case 'EAI_AGAIN':
         case 'ER_DBACCESS_DENIED_ERROR':
         case 'ER_BAD_DB_ERROR':
             return new DbConnectionError(`Database does not exists or you don\'t have access to it, sql message: ${err.sqlMessage}`)
         case 'ER_ACCESS_DENIED_ERROR':
             return new DbConnectionError(`Access to database denied - probably wrong credentials, sql message:  ${err.sqlMessage} `)
+        case 'EAI_AGAIN':
         case 'ENOTFOUND':
             return new DbConnectionError(`Access to database denied - host is unavailable, sql message:  ${err.sqlMessage} `)
         default :
