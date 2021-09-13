286909fea66d5524d9e225d982b10fd4f5b0fc25
Ido <idokah@wix.com>
2021-09-13 17:27:24 +0300

    mongo data provider implementation

--- packages/external-db-mongo/lib/mongo_data_provider.js
+++ packages/external-db-mongo/lib/mongo_data_provider.js
@@ -1,114 +1,73 @@
-// const { escapeId, validateLiteral, escape, patchFieldName } = require('./mssql_utils')
+ const { dbName } = require('./mongo_utils')
 // const { SystemFields } = require('velo-external-db-commons')
 // const { translateErrorCodes } = require('./sql_exception_translator')
 
 class DataProvider {
-    constructor(pool, filterParser) {
+    constructor(client, filterParser) {
+        this.client = client
         this.filterParser = filterParser
-        this.sql = pool
+        this.pool = this.client.db(dbName(client))
     }
 
-    // async find(collectionName, filter, sort, skip, limit) {
-    //     const {filterExpr, parameters} = this.filterParser.transform(filter)
-    //     const {sortExpr} = this.filterParser.orderBy(sort)
-    //     const pagingQueryStr = this.pagingQueryFor(skip, limit)
-    //
-    //     const sql = `SELECT * FROM ${escapeId(collectionName)} ${filterExpr} ${sortExpr} ${pagingQueryStr}`
-    //
-    //     return await this.query(sql, parameters)
-    // }
-    //
-    // async count(collectionName, filter) {
-    //     const {filterExpr, parameters} = this.filterParser.transform(filter)
-    //
-    //     const sql = `SELECT COUNT(*) as num FROM ${escapeId(collectionName)} ${filterExpr}`
-    //     const rs = await this.query(sql, parameters)
-    //
-    //     return rs[0]['num']
-    // }
-    //
-    // patch(item) {
-    //     return Object.entries(item).reduce((o, [k, v]) => Object.assign({}, o, {[patchFieldName(k)]: v}), {})
-    // }
-    //
-    // // todo: check if we can get schema in a safer way. should be according to schema of the table
-    // async insert(collectionName, items) {
-    //     const rss = await Promise.all(items.map(item => this.insertSingle(collectionName, item)))
-    //
-    //     return rss.reduce((s, rs) => s + rs, 0)
-    // }
-    //
-    // insertSingle(collectionName, item) {
-    //     const sql = `INSERT INTO ${escapeId(collectionName)} (${Object.keys(item).map( escapeId ).join(', ')}) VALUES (${Object.keys(item).map( validateLiteral ).join(', ')})`
-    //     return this.query(sql, this.patch(item), true)
-    // }
-    //
-    // async update(collectionName, items) {
-    //     const rss = await Promise.all(items.map(item => this.updateSingle(collectionName, item)))
-    //     return rss.reduce((s, rs) => s + rs, 0)
-    // }
-    //
-    // async updateSingle(collectionName, item) {
-    //     const systemFieldNames = SystemFields.map(f => f.name)
-    //     const updateFields = Object.keys(item).filter( k => !systemFieldNames.includes(k) )
-    //
-    //     if (updateFields.length === 0) {
-    //         return 0
-    //     }
-    //
-    //     const sql = `UPDATE ${escapeId(collectionName)} SET ${updateFields.map(f => `${escapeId(f)} = ${validateLiteral(f)}`).join(', ')} WHERE _id = ${validateLiteral('_id')}`
-    //
-    //     return await this.query(sql, this.patch(item), true)
-    // }
-    //
-    //
-    // async delete(collectionName, itemIds) {
-    //     const sql = `DELETE FROM ${escapeId(collectionName)} WHERE _id IN (${itemIds.map((t, i) => validateLiteral(`_id${i}`)).join(', ')})`
-    //     const rs = await this.query(sql, itemIds.reduce((p, t, i) => Object.assign({}, p, { [patchFieldName(`_id${i}`)]: t }), {}), true)
-    //                          .catch( translateErrorCodes )
-    //     return rs
-    // }
-    //
-    // async truncate(collectionName) {
-    //     await this.sql.query(`TRUNCATE TABLE ${escapeId(collectionName)}`).catch( translateErrorCodes )
-    // }
-    //
-    // async aggregate(collectionName, filter, aggregation) {
-    //     const {filterExpr: whereFilterExpr, parameters: whereParameters} = this.filterParser.transform(filter)
-    //     const {fieldsStatement, groupByColumns, havingFilter, parameters} = this.filterParser.parseAggregation(aggregation.processingStep, aggregation.postFilteringStep)
-    //
-    //     const sql = `SELECT ${fieldsStatement} FROM ${escapeId(collectionName)} ${whereFilterExpr} GROUP BY ${groupByColumns.map( escapeId ).join(', ')} ${havingFilter}`
-    //
-    //     return await this.query(sql, Object.assign({}, whereParameters, parameters))
-    // }
-    //
-    // async query(sql, parameters, op) {
-    //     const request = Object.entries(parameters)
-    //                           .reduce((r, [k, v]) => r.input(k, v),
-    //                                   this.sql.request())
-    //
-    //     const rs = await request.query(sql)
-    //                             .catch( translateErrorCodes )
-    //
-    //     if (op) {
-    //         return rs.rowsAffected[0]
-    //     }
-    //     return rs.recordset
-    // }
-    //
-    // pagingQueryFor(skip, limit) {
-    //     let offsetSql = ''
-    //     if (skip > 0) {
-    //         offsetSql = `OFFSET ${escape(skip)} ROWS`
-    //     }
-    //     let limitSql = ''
-    //     if (skip > 0) {
-    //         limitSql = `FETCH NEXT ${escape(limit)} ROWS ONLY`
-    //     }
-    //     return `${offsetSql} ${limitSql}`.trim()
-    // }
+    async find(collectionName, filter, sort, skip, limit) {
+        const { filterExpr } = this.filterParser.transform(filter)
+        const { sortExpr } = this.filterParser.orderBy(sort)
 
+        return await this.pool.collection(collectionName).find(filterExpr,
+            Object.assign({}, { skip, limit }, sortExpr))
+            .toArray()
+    }
+
+    async count(collectionName, filter) {
+        const { filterExpr } = this.filterParser.transform(filter)
+
+        const result = await this.pool.collection(collectionName).count(filterExpr)
+        return result
+    }
+
+    async insert(collectionName, items) {
+        const rss = await this.pool.collection(collectionName).insertMany(items)
+        return rss.insertedCount
+    }
+
+    async update(collectionName, items) {
+
+        const rss = await Promise.all(items.map(item => this.updateSingle(collectionName, item)))
+
+        return rss.map((r) => r.modifiedCount).reduce((s, rs) => s + rs)
+    }
+
+    async updateSingle(collectionName, item) {
+        return await this.pool.collection(collectionName).updateOne({ _id: item._id },
+            { $set: { ...item } })
+    }
+
+    async delete(collectionName, itemIds) {
+        const rss = await this.pool.collection(collectionName).remove({ _id: { $in: itemIds } })
+        return rss.deletedCount
+    }
+
+    async truncate(collectionName) {
+        await this.pool.collection(collectionName).remove({})
+    }
+
+    async aggregate(collectionName, filter, aggregation) {
+        const { fieldsStatement, groupByColumns, havingFilter, parameters } = this.filterParser.parseAggregation(aggregation.processingStep, aggregation.postFilteringStep)
+        const { filterExpr } = this.filterParser.transform(filter)
+
+        return await this.pool.collection(collectionName).aggregate( [
+            { $match: filterExpr },
+            {
+              $group: fieldsStatement
+            },
+            { $match: havingFilter }
+         ] ).toArray()
+        return result;
+    }
 
 }
+
+
+
 
 module.exports = DataProvider
--- packages/external-db-mongo/lib/mongo_utils.js
+++ packages/external-db-mongo/lib/mongo_utils.js
@@ -4,5 +4,6 @@ const escapeId = s => s //SqlString.escapeId(s)
 const escape = s => s //SqlString.escape(s)
 const patchFieldName = s => s //`x${SqlString.escape(s).substring(1).slice(0, -1)}`
 const validateLiteral = s => s //`@${patchFieldName(s)}`
+const dbName = client => client.topology.s.options.dbName
 
-module.exports = { escapeId, validateLiteral, patchFieldName, escape }
+module.exports = { escapeId, validateLiteral, patchFieldName, escape, dbName }
