a2d32d9ebe7351cb98b752ff9f0241763d6e75bb
Idokah <82806105+Idokah@users.noreply.github.com>
2021-09-14 17:48:02 +0300

    mongo data provider fixes (#33)
    * resolve conflicts
    
    * change update method to bulkWrite instead updateSingle each
    
    * change name

--- packages/external-db-mongo/lib/mongo_data_provider.js
+++ packages/external-db-mongo/lib/mongo_data_provider.js
@@ -1,4 +1,3 @@
- const { dbName } = require('./mongo_utils')
 // const { SystemFields } = require('velo-external-db-commons')
 // const { translateErrorCodes } = require('./sql_exception_translator')
 
@@ -6,7 +5,7 @@ class DataProvider {
     constructor(client, filterParser) {
         this.client = client
         this.filterParser = filterParser
-        this.pool = this.client.db(dbName(client))
+        this.pool = this.client.db()
     }
 
     async find(collectionName, filter, sort, skip, limit) {
@@ -34,16 +33,19 @@ class DataProvider {
     }
 
     async update(collectionName, items) {
-        const rss = await Promise.all(items.map(item => this.updateSingle(collectionName, item)))
-
-        return rss.map((r) => r.modifiedCount).reduce((s, rs) => s + rs)
+        const result = await this.pool.collection(collectionName)
+                                    .bulkWrite(items.map((item)=>this.updateSingleObj(item)))
+        return result.nModified
     }
 
-    async updateSingle(collectionName, item) {
-        return await this.pool.collection(collectionName)
-                              .updateOne({ _id: item._id },
-                                         { $set: { ...item } })
+    updateSingleObj(item){
+        return { updateOne: 
+                    {
+                        'filter': { '_id': item._id },
+                        'update': { '$set': { ...item } }
+                    }}
     }
+
 
     async delete(collectionName, itemIds) {
         const rss = await this.pool.collection(collectionName)
@@ -61,11 +63,11 @@ class DataProvider {
         const { filterExpr } = this.filterParser.transform(filter)
 
         return await this.pool.collection(collectionName)
-                              .aggregate( [ { $match: filterExpr },
-                                            { $group: fieldsStatement },
-                                            { $match: havingFilter }
-                                          ] )
-                              .toArray()
+                                .aggregate( [ { $match: filterExpr },
+                                                fieldsStatement,
+                                                havingFilter
+                                            ] )
+                                .toArray()
     }
 }
 
--- packages/external-db-mongo/lib/mongo_utils.js
+++ packages/external-db-mongo/lib/mongo_utils.js
@@ -4,6 +4,5 @@ const escapeId = s => s //SqlString.escapeId(s)
 const escape = s => s //SqlString.escape(s)
 const patchFieldName = s => s //`x${SqlString.escape(s).substring(1).slice(0, -1)}`
 const validateLiteral = s => s //`@${patchFieldName(s)}`
-const dbName = client => client.db().databaseName
 
-module.exports = { escapeId, validateLiteral, patchFieldName, escape, dbName }
+module.exports = { escapeId, validateLiteral, patchFieldName, escape }
