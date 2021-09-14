46961599d778fb2afd7c5831a6a93d53d5d4b24f
Noam Almog <noama@wix.com>
2021-09-14 10:33:28 +0300

    indentation

--- packages/external-db-mongo/lib/mongo_data_provider.js
+++ packages/external-db-mongo/lib/mongo_data_provider.js
@@ -13,59 +13,60 @@ class DataProvider {
         const { filterExpr } = this.filterParser.transform(filter)
         const { sortExpr } = this.filterParser.orderBy(sort)
 
-        return await this.pool.collection(collectionName).find(filterExpr,
-            Object.assign({}, { skip, limit }, sortExpr))
-            .toArray()
+        return await this.pool.collection(collectionName)
+                              .find(filterExpr,
+                                    Object.assign({}, { skip, limit }, sortExpr))
+                              .toArray()
     }
 
     async count(collectionName, filter) {
         const { filterExpr } = this.filterParser.transform(filter)
 
-        const result = await this.pool.collection(collectionName).count(filterExpr)
+        const result = await this.pool.collection(collectionName)
+                                      .count(filterExpr)
         return result
     }
 
     async insert(collectionName, items) {
-        const rss = await this.pool.collection(collectionName).insertMany(items)
+        const rss = await this.pool.collection(collectionName)
+                                   .insertMany(items)
         return rss.insertedCount
     }
 
     async update(collectionName, items) {
-
         const rss = await Promise.all(items.map(item => this.updateSingle(collectionName, item)))
 
         return rss.map((r) => r.modifiedCount).reduce((s, rs) => s + rs)
     }
 
     async updateSingle(collectionName, item) {
-        return await this.pool.collection(collectionName).updateOne({ _id: item._id },
-            { $set: { ...item } })
+        return await this.pool.collection(collectionName)
+                              .updateOne({ _id: item._id },
+                                         { $set: { ...item } })
     }
 
     async delete(collectionName, itemIds) {
-        const rss = await this.pool.collection(collectionName).remove({ _id: { $in: itemIds } })
+        const rss = await this.pool.collection(collectionName)
+                                   .remove({ _id: { $in: itemIds } })
         return rss.deletedCount
     }
 
     async truncate(collectionName) {
-        await this.pool.collection(collectionName).remove({})
+        await this.pool.collection(collectionName)
+                       .remove({})
     }
 
     async aggregate(collectionName, filter, aggregation) {
         const { fieldsStatement, havingFilter } = this.filterParser.parseAggregation(aggregation.processingStep, aggregation.postFilteringStep)
         const { filterExpr } = this.filterParser.transform(filter)
 
-        return await this.pool.collection(collectionName).aggregate( [
-            { $match: filterExpr },
-            {
-              $group: fieldsStatement
-            },
-            { $match: havingFilter }
-         ] ).toArray()
+        return await this.pool.collection(collectionName)
+                              .aggregate( [ { $match: filterExpr },
+                                            { $group: fieldsStatement },
+                                            { $match: havingFilter }
+                                          ] )
+                              .toArray()
     }
 }
-
-
-
 
 module.exports = DataProvider
