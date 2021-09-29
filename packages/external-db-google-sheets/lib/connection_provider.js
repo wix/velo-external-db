fcbcb10a0e7f5edbb311dd5a3825286f428cc266
Max Polsky <maxim.polski@gmail.com>
2021-09-29 16:52:53 +0300

    new operation class for google-sheets

--- packages/external-db-google-sheets/lib/connection_provider.js
+++ packages/external-db-google-sheets/lib/connection_provider.js
@@ -1,6 +1,8 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const SchemaProvider = require('./google_sheet_schema_provider')
 const DataProvider = require('./google_sheet_data_provider')
+const DatabaseOperations = require('./google_sheet_operations')
+
 
 const init = async(cfg) => {
     const doc = new GoogleSpreadsheet(cfg.sheetId)
@@ -9,10 +11,11 @@ const init = async(cfg) => {
         private_key: cfg.apiPrivateKey
     })
 
+    const databaseOperations = new DatabaseOperations(doc)
     const dataProvider = new DataProvider(doc)
     const schemaProvider = new SchemaProvider(doc)
 
-    return { dataProvider, schemaProvider /*, databaseOperations, connection: firestore, cleanup: async() => await firestore.terminate()*/ }
+    return { dataProvider, schemaProvider, databaseOperations /*, connection: firestore, cleanup: async() => await firestore.terminate()*/ }
 }
 
 
---
+++ packages/external-db-google-sheets/lib/google_sheet_operations.js
@@ -0,0 +1,15 @@
+
+class DatabaseOperations {
+    constructor(doc) {
+        this.doc = doc
+    }
+
+    async validateConnection() {
+        return await this.doc.loadInfo()
+        .then(() => { return { valid: true } })
+        .catch((e) => { return { valid: false, error: e } })
+    }
+
+}
+
+module.exports = DatabaseOperations
