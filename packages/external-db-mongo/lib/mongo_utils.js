d2d1c758a9244b38974b36576e1fac2eb7cc31cd
Idokah <82806105+Idokah@users.noreply.github.com>
2021-09-29 14:11:00 +0300

    mongo-filter_transformer without urlized yet (#35)

--- packages/external-db-mongo/lib/mongo_utils.js
+++ packages/external-db-mongo/lib/mongo_utils.js
@@ -4,5 +4,6 @@ const escapeId = s => s //SqlString.escapeId(s)
 const escape = s => s //SqlString.escape(s)
 const patchFieldName = s => s //`x${SqlString.escape(s).substring(1).slice(0, -1)}`
 const validateLiteral = s => s //`@${patchFieldName(s)}`
+const EMPTY_FILTER = {filterExpr:{}}
 
-module.exports = { escapeId, validateLiteral, patchFieldName, escape }
+module.exports = { escapeId, validateLiteral, patchFieldName, escape, EMPTY_FILTER }
--- packages/external-db-mongo/lib/sql_filter_transformer.js
+++ packages/external-db-mongo/lib/sql_filter_transformer.js
@@ -1,252 +1,146 @@
-// const { InvalidQuery } = require('velo-external-db-commons').errors
-// const { EMPTY_FILTER, EMPTY_SORT, isObject } = require('velo-external-db-commons')
-// const { escapeId, validateLiteral, patchFieldName, escapeFieldId } = require('./mssql_utils')
+const { InvalidQuery } = require('velo-external-db-commons').errors
+const { EMPTY_SORT, isObject } = require('velo-external-db-commons')
+const { EMPTY_FILTER } = require('./mongo_utils')
 
 class FilterParser {
     constructor() {
     }
 
-    // transform(filter) {
-    //     const results = this.parseFilter(filter, 1, {})
-    //
-    //     if (results.length === 0) {
-    //         return EMPTY_FILTER;
-    //     }
-    //
-    //     return {
-    //         filterExpr: `WHERE ${results[0].filterExpr}`,
-    //         parameters: results[0].parameters
-    //     };
-    // }
-    //
-    // wixDataFunction2Sql(f) {
-    //     switch (f) {
-    //         case '$avg':
-    //             return 'AVG'
-    //         case '$max':
-    //             return 'MAX'
-    //         case '$min':
-    //             return 'MIN'
-    //         case '$sum':
-    //             return 'SUM'
-    //         default:
-    //             throw new InvalidQuery(`Unrecognized function ${f}`)
-    //     }
-    // }
-    //
-    // parseAggregation(aggregation, postFilter) {
-    //     const groupByColumns = []
-    //     const filterColumnsStr = []
-    //     if (isObject(aggregation._id)) {
-    //         filterColumnsStr.push(...Object.values(aggregation._id).map(f => escapeId(f)))
-    //         groupByColumns.push(...Object.values(aggregation._id))
-    //     } else {
-    //         filterColumnsStr.push(escapeId(aggregation._id))
-    //         groupByColumns.push(aggregation._id)
-    //     }
-    //
-    //     const aliasToFunction = {}
-    //     Object.keys(aggregation)
-    //           .filter(f => f !== '_id')
-    //           .forEach(fieldAlias => {
-    //               Object.entries(aggregation[fieldAlias])
-    //                     .forEach(([func, field]) => {
-    //                         filterColumnsStr.push(`${this.wixDataFunction2Sql(func)}(${escapeId(field)}) AS ${escapeId(fieldAlias)}`)
-    //                         aliasToFunction[fieldAlias] = `${this.wixDataFunction2Sql(func)}(${escapeId(field)})`
-    //                     })
-    //           })
-    //
-    //     const havingFilter = this.parseFilter(postFilter, aliasToFunction)
-    //
-    //     const {filterExpr, parameters} =
-    //         havingFilter.map(({filterExpr, parameters}) => ({ filterExpr: filterExpr !== '' ? `HAVING ${filterExpr}` : '',
-    //                                                           parameters: parameters}))
-    //                     .concat({ filterExpr: '', parameters: {}})[0]
-    //
-    //
-    //     return {
-    //         fieldsStatement: filterColumnsStr.join(', '),
-    //         groupByColumns,
-    //         havingFilter: filterExpr,
-    //         parameters: parameters,
-    //     }
-    // }
-    //
-    // parseFilter(filter, inlineFields) {
-    //     if (!filter || !isObject(filter)|| filter.operator === undefined) {
-    //         return []
-    //     }
-    //
-    //     switch (filter.operator) {
-    //         case '$and':
-    //         case '$or':
-    //             const res = filter.value.reduce((o, f) => {
-    //                 const res = this.parseFilter.bind(this)(f, inlineFields)
-    //                 return {
-    //                     filter: o.filter.concat( ...res ),
-    //                 }
-    //             }, { filter: []})
-    //             const op = filter.operator === '$and' ? ' AND ' : ' OR '
-    //             return [{
-    //                 filterExpr: res.filter.map(r => r.filterExpr).join( op ),
-    //                 parameters: res.filter.reduce((o, s) => Object.assign({}, o, s.parameters), {} )
-    //             }]
-    //         case '$not':
-    //             const res2 = this.parseFilter( filter.value, inlineFields )
-    //             return [{
-    //                 filterExpr: `NOT (${res2[0].filterExpr})`,
-    //                 parameters: res2[0].parameters
-    //             }]
-    //     }
-    //
-    //     if (this.isSingleFieldOperator(filter.operator)) {
-    //         const params = this.valueForOperator(filter.fieldName, filter.value, filter.operator)
-    //
-    //         return [{
-    //             filterExpr: `${this.inlineVariableIfNeeded(filter.fieldName, inlineFields)} ${this.veloOperatorToMySqlOperator(filter.operator, filter.value)} ${params.sql}`.trim(),
-    //             parameters: this.parametersFor(filter.fieldName, filter.value)
-    //         }]
-    //     }
-    //
-    //
-    //     if (this.isSingleFieldStringOperator(filter.operator)) {
-    //         return [{
-    //             filterExpr: `${this.inlineVariableIfNeeded(filter.fieldName, inlineFields)} LIKE ${validateLiteral(filter.fieldName)}`,
-    //             parameters: { [patchFieldName(filter.fieldName)]: this.valueForStringOperator(filter.operator, filter.value)}
-    //         }]
-    //     }
-    //
-    //     if (filter.operator === '$urlized') {
-    //         return [{
-    //             filterExpr: `LOWER(${escapeId(filter.fieldName)}) LIKE ${validateLiteral(filter.fieldName)}`,
-    //             parameters: { [patchFieldName(filter.fieldName)]: filter.value.map(s => s.toLowerCase()).join('[- ]') }
-    //         }]
-    //     }
-    //
-    //     return []
-    // }
-    //
-    // parametersFor(name, value) {
-    //     if (value !== undefined) {
-    //         if (!Array.isArray(value)) {
-    //             return { [patchFieldName(name)]: this.patchTrueFalseValue(value) }
-    //         } else {
-    //             return value.reduce((o, v, i) => Object.assign({}, o, { [patchFieldName(`${name}${i + 1}`)]: v}), {})
-    //         }
-    //     }
-    //     return { }
-    // }
-    //
-    // valueForStringOperator(operator, value) {
-    //     switch (operator) {
-    //         case '$contains':
-    //             return `%${value}%`
-    //         case '$startsWith':
-    //             return `${value}%`
-    //         case '$endsWith':
-    //             return `%${value}`
-    //     }
-    // }
-    //
-    // isSingleFieldOperator(operator) {
-    //     return ['$ne', '$lt', '$lte', '$gt', '$gte', '$hasSome', '$eq'].includes(operator)
-    // }
-    //
-    // isSingleFieldStringOperator(operator) {
-    //     return ['$contains', '$startsWith', '$endsWith'].includes(operator)
-    // }
-    //
-    // prepareStatementVariables(n, fieldName) {
-    //     return Array.from({length: n}, (_, i) => validateLiteral(`${fieldName}${i + 1}`) )
-    //                 .join(', ')
-    // }
-    //
-    //
-    // valueForOperator(fieldName, value, operator) {
-    //     if (operator === '$hasSome') {
-    //         if (value === undefined || value.length === 0) {
-    //             throw new InvalidQuery('$hasSome cannot have an empty list of arguments')
-    //         }
-    //         return {
-    //             sql: `(${this.prepareStatementVariables(value.length, fieldName)})`,
-    //         }
-    //     } else if (operator === '$eq' && value === undefined) {
-    //         return {
-    //             sql: '',
-    //         }
-    //     }
-    //
-    //     return {
-    //         sql: validateLiteral(fieldName),
-    //     }
-    // }
-    //
-    // veloOperatorToMySqlOperator(operator, value) {
-    //     switch (operator) {
-    //         case '$eq':
-    //             if (value !== undefined) {
-    //                 return '='
-    //             }
-    //             return 'IS NULL'
-    //         case '$ne':
-    //             return '<>'
-    //         case '$lt':
-    //             return '<'
-    //         case '$lte':
-    //             return '<='
-    //         case '$gt':
-    //             return '>'
-    //         case '$gte':
-    //             return '>='
-    //         case '$hasSome':
-    //             return 'IN'
-    //     }
-    // }
-    //
-    // orderBy(sort) {
-    //     if (!Array.isArray(sort) || !sort.every(isObject)) {
-    //         return EMPTY_SORT;
-    //     }
-    //
-    //     const results = sort.flatMap( this.parseSort )
-    //
-    //     if (results.length === 0) {
-    //         return EMPTY_SORT;
-    //     }
-    //
-    //     return {
-    //         sortExpr: `ORDER BY ${results.map( s => s.expr).join(', ')}`
-    //     }
-    // }
-    //
-    // parseSort({ fieldName, direction }) {
-    //     if (typeof fieldName !== 'string') {
-    //         return []
-    //     }
-    //     const _direction = direction || 'ASC'
-    //
-    //     const dir = 'ASC' === _direction.toUpperCase() ? 'ASC' : 'DESC';
-    //
-    //     return [{
-    //         expr: `${escapeId(fieldName)} ${dir}`
-    //     }]
-    // }
-    //
-    // patchTrueFalseValue(value) {
-    //     if (value === true || value === false) {
-    //         return value ? 1 : 0
-    //     }
-    //     return value
-    // }
-    //
-    // inlineVariableIfNeeded(fieldName, inlineFields) {
-    //     if (inlineFields) {
-    //         if (inlineFields[fieldName]) {
-    //             return inlineFields[fieldName]
-    //         }
-    //     }
-    //     return escapeId(fieldName)
-    // }
+    transform(filter) {
+        const results = this.parseFilter(filter)
+        if (results.length === 0) {
+            return EMPTY_FILTER;
+        }
+        return {
+            filterExpr: results[0].filterExpr
+        };
+    }
+
+    parseAggregation(aggregation, postFilter) {
+        const havingFilter = this.parseFilter(postFilter)
+        const fieldsStatement = {}
+        if (isObject(aggregation._id)) {
+            aggregation._id = Object.keys(aggregation._id).reduce((r, c) =>
+                Object.assign(r, { [aggregation._id[c]]: `$${aggregation._id[c]}` })
+                , {})
+            Object.assign(fieldsStatement, { _id: aggregation._id })
+        }
+        else {
+            Object.assign(fieldsStatement, { [aggregation._id]: `$${aggregation._id}` })
+        }
+        Object.keys(aggregation)
+            .filter(f => f !== '_id')
+            .forEach(fieldAlias => {
+                Object.entries(aggregation[fieldAlias])
+                    .forEach(([func, field]) => {
+                        Object.assign(fieldsStatement, { [fieldAlias]: { [func]: `$${field}` } })
+                    })
+            })
+
+        const filterObj = (havingFilter.reduce(((r, c) => Object.assign(r, c)), {}))
+
+        return {
+            fieldsStatement: { '$group': fieldsStatement },
+            havingFilter: { '$match': filterObj.filterExpr || {} },
+        }
+    }
+
+    parseFilter(filter, inlineFields) {
+        
+        if (!filter || !isObject(filter) || filter.operator === undefined) {
+            return []
+        }
+        const operator = this.veloOperatorToMongoOperator(filter.operator)
+
+        if (this.isMultipleFieldOperator(operator)) {
+            const res = filter.value.map(this.parseFilter.bind(this))
+            return [{ filterExpr: { [`${operator}`]: res.map(r => r[0].filterExpr) } }]
+        }
+
+        if (operator === '$not') {
+            const res = this.parseFilter(filter.value)
+            return [{ filterExpr: { [`${operator}`]: res[0].filterExpr } }]
+        }
+
+        if (this.isSingleFieldStringOperator(operator)) {
+            return [{ filterExpr: { [`${filter.fieldName}`]: { '$regex': this.valueForStringOperator(operator, filter.value) } } }]
+        }
+
+        return [{ filterExpr: { [`${filter.fieldName}`]: { [`${operator}`]: this.valueForOperator(filter.value, operator) } } }]
+
+    }
+
+    isMultipleFieldOperator(operator) {
+        return ['$and', '$or'].includes(operator)
+    }
+
+    valueForStringOperator(operator, value) {
+        switch (operator) {
+            case '$contains':
+                return `${value}`
+            case '$startsWith':
+                return `^${value}`
+            case '$endsWith':
+                return `${value}$`
+        }
+    }
+
+    isSingleFieldStringOperator(operator) {
+        return ['$contains', '$startsWith', '$endsWith'].includes(operator)
+    }
+
+
+    valueForOperator(value, operator) {
+        if (operator === '$in') {
+            if (value === undefined || value.length === 0) {
+                throw new InvalidQuery('$hasSome cannot have an empty list of arguments')
+            }
+            return value
+        }
+        else if (operator === '$eq' && value === undefined) {
+            return null
+        }
+
+        return value
+    }
+
+    veloOperatorToMongoOperator(operator, value) {
+        switch (operator) {
+            case '$hasSome':
+                return '$in'
+            default:
+                return operator
+        }
+        //filter.operator === '$hasSome' || filter.operator === '$startsWith' || filter.operator === '$endsWith' || filter.operator === '$urlized'
+    }
+
+    orderBy(sort) {
+        if (!Array.isArray(sort) || !sort.every(isObject)) {
+            return EMPTY_SORT;
+        }
+
+        const results = sort.flatMap(this.parseSort)
+        if (results.length === 0) {
+            return EMPTY_SORT;
+        }
+        return {
+            sortExpr: { sort: results.map(result => result.expr) }
+        }
+    }
+
+    parseSort({ fieldName, direction }) {
+        if (typeof fieldName !== 'string') {
+            return []
+        }
+        const _direction = direction || 'ASC'
+
+        const dir = 'ASC' === _direction.toUpperCase() ? 'asc' : 'desc';
+
+        return {
+            expr: [`${fieldName}`, dir]
+        }
+    }
+
 }
 
 module.exports = FilterParser
