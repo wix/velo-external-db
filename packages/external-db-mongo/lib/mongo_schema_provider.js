ac2ef8ffd8495f062a7ecb0c75df1acfc87f5943
Noam Almog <noama@wix.com>
2021-10-03 12:48:13 +0300

    refactor

--- packages/external-db-mongo/lib/mongo_schema_provider.js
+++ packages/external-db-mongo/lib/mongo_schema_provider.js
@@ -16,25 +16,30 @@ class SchemaProvider {
     }
 
     async list() {
-        const resp = await this.client.db().collection(SystemTable).find({})
+        const resp = await this.client.db()
+                                      .collection(SystemTable)
+                                      .find({})
         const l = await resp.toArray()
-        const tables = l.reduce((o, d) => Object.assign( o, { [d._id]: { fields: d.fields } }), {})
+        const tables = l.reduce((o, d) => ({ ...o, [d._id]: { fields: d.fields } }), {})
         return Object.entries(tables)
                      .map(([collectionName, rs]) => asWixSchema([...SystemFields, ...rs.fields].map( this.reformatFields.bind(this) ), collectionName))
     }
 
     async create(collectionName, columns) {
-        const collection = await this.client.db().collection(SystemTable).findOne({ _id: collectionName })
+        const collection = await this.collectionDataFor(collectionName)
         if (!collection) {
-            await this.client.db().collection(SystemTable).insertOne( { _id: collectionName, fields: columns || [] })
-            await this.client.db().createCollection(collectionName)
+            await this.client.db()
+                             .collection(SystemTable)
+                             .insertOne( { _id: collectionName, fields: columns || [] })
+            await this.client.db()
+                             .createCollection(collectionName)
         }
     }
 
     async addColumn(collectionName, column) {
         await validateSystemFields(column.name)
 
-        const collection = await this.client.db().collection(SystemTable).findOne({ _id: collectionName })
+        const collection = await this.collectionDataFor(collectionName)
         if (!collection) {
             throw new CollectionDoesNotExists('Collection does not exists')
         }
@@ -44,14 +49,16 @@ class SchemaProvider {
             throw new FieldAlreadyExists('Collection already has a field with the same name')
         }
 
-        await this.client.db().collection(SystemTable)
-                              .updateOne({ _id: collectionName }, { $set: { fields: [...fields, column] } })
+        await this.client.db()
+                         .collection(SystemTable)
+                         .updateOne({ _id: collectionName },
+                                    { $set: { fields: [...fields, column] } })
     }
 
     async removeColumn(collectionName, columnName) {
         await validateSystemFields(columnName)
 
-        const collection = await this.client.db().collection(SystemTable).findOne({ _id: collectionName })
+        const collection = await this.collectionDataFor(collectionName)
         if (!collection) {
             throw new CollectionDoesNotExists('Collection does not exists')
         }
@@ -61,12 +68,14 @@ class SchemaProvider {
             throw new FieldDoesNotExist('Collection does not contain a field with this name')
         }
 
-        await this.client.db().collection(SystemTable)
-                              .updateOne({ _id: collectionName }, { $set: { fields: fields.filter(f => f.name !== columnName) } })
+        await this.client.db()
+                         .collection(SystemTable)
+                         .updateOne({ _id: collectionName },
+                                    { $set: { fields: fields.filter(f => f.name !== columnName) } })
     }
 
     async describeCollection(collectionName) {
-        const collection = await this.client.db().collection(SystemTable).findOne({ _id: collectionName })
+        const collection = await this.collectionDataFor(collectionName)
         if (!collection) {
             throw new CollectionDoesNotExists('Collection does not exists')
         }
@@ -75,11 +84,20 @@ class SchemaProvider {
     }
 
     async drop(collectionName) {
-        const d = await this.client.db().collection(SystemTable).deleteOne( { _id: collectionName })
+        const d = await this.client.db()
+                                   .collection(SystemTable)
+                                   .deleteOne( { _id: collectionName })
         if (d.deletedCount === 1) {
-            await this.client.db().collection(collectionName).drop()
-                             .catch(e => console.log(e))
+            await this.client.db()
+                             .collection(collectionName)
+                             .drop()
         }
+    }
+
+    async collectionDataFor(collectionName) {
+        return await this.client.db()
+                                .collection(SystemTable)
+                                .findOne({ _id: collectionName })
     }
 }
 
