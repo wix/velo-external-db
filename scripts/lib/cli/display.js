d5905bafd9cc770e80507588d6f98620a8d9cc16
Noam Almog <noama@wix.com>
2021-10-04 17:20:51 +0300

    added some script progress to script

---
+++ scripts/lib/cli/display.js
@@ -0,0 +1,46 @@
+const Spinner = require('cli-spinner').Spinner
+const { green, grey, greenBright, redBright } = require('chalk')
+
+const startSpinnerWith = async (msg, f, completeMsg) => {
+    const spinner = new Spinner({
+        text: `\t\t %s ${msg}`,
+        stream: process.stderr,
+        onTick: function(msg){
+            this.clearLine(this.stream);
+            this.stream.write(msg);
+        }
+    })
+
+    spinner.setSpinnerString(18)
+
+    spinner.start()
+
+    try {
+        const res = await f()
+
+        spinner.stop(true)
+
+        process.stderr.write(`\t\t ${greenBright('✓')} ${grey(completeMsg || msg)}\n`)
+
+        return res
+    } catch (e) {
+        spinner.stop(true)
+        process.stderr.write(`\t\t ${redBright('✓')} ${grey(completeMsg || msg)}\n`)
+        process.stderr.write(redBright('Process failed\n'))
+        process.exit(1)
+    }
+
+}
+/*
+
+await startSpinnerWith(`Creating demo collection`, () => createCollection('demo_collection', secretKey, serviceUrl))
+    console.log('')
+    console.log('')
+    console.log(`${green('[INFO]:')} Provision Cloud Sql and Cloud Run instances using Wix GCP`)
+
+ */
+
+const info = msg => console.log(`${green('[INFO]:')} ${msg}`)
+const blankLine = () => console.log('')
+
+module.exports = { startSpinnerWith, info, blankLine }
--- scripts/lib/core/provision.js
+++ scripts/lib/core/provision.js
@@ -1,6 +1,7 @@
 const aws = require('../aws')
 const { randomCredentials, randomSecretKey } = require('../utils/password_utils')
 const { blockUntil } = require('../utils/utils')
+const { info, blankLine, startSpinnerWith } = require('../cli/display')
 
 const providerFor = (vendor) => {
     switch (vendor) {
@@ -17,39 +18,47 @@ const provisionDb = async (provider, engine, configWriter) => {
     const instanceName = `velo-external-db-${number}`
     const secretKey = randomSecretKey()
 
-    await provider.createDb({ name: instanceName, engine: engine, credentials: dbCredentials})
-    await blockUntil( async () => !(await provider.dbStatusAvailable(instanceName)).available )
+    await startSpinnerWith(`Creating ${engine} DB Instance`, async () => await provider.createDb({ name: instanceName, engine: engine, credentials: dbCredentials}))
+    await startSpinnerWith(`Waiting for db instance to start`, async () => await blockUntil( async () => !(await provider.dbStatusAvailable(instanceName)).available ))
+
     const status = await provider.dbStatusAvailable(instanceName)
 
     const dbName = 'velo_db'
 
-    await configWriter.writeConfig(dbCredentials, status.host, dbName, secretKey)
+    await startSpinnerWith(`Writing db config`, async () => await configWriter.writeConfig(dbCredentials, status.host, dbName, secretKey))
 
-    await provider.postCreateDb(dbName, status.host, dbCredentials)
+    await startSpinnerWith(`Provision Velo DB on db instance`, async () => await provider.postCreateDb(dbName, status.host, dbCredentials))
 }
 
 const provisionAdapter = async (provider, engine) => {
     const number = Math.floor(Math.random() * 100)
     const instanceName = `velo-external-db-adapter-${number}`
 
-    const { serviceId } = await provider.createAdapter(instanceName, engine)
-    await blockUntil( async () => !(await provider.adapterStatus(serviceId)).available )
+    const { serviceId } = await startSpinnerWith(`Provision Adapter`, async () => await provider.createAdapter(instanceName, engine))
+    await startSpinnerWith(`Waiting adapter server instance to start`, async () => await blockUntil( async () => !(await provider.adapterStatus(serviceId)).available ))
+
     const status = await provider.adapterStatus(serviceId)
 
-    console.log(status.serviceUrl)
+    blankLine()
+    info(`Adapter available at ${status.serviceUrl}`)
 }
 
 
 const main = async ({ vendor, engine, credentials }) => {
-    console.log(vendor, engine)
-
     const provider = providerFor(vendor, credentials)
 
     const configWriter = new provider.ConfigWriter(credentials)
     const dbProvision = new provider.DbProvision(credentials)
     const adapterProvision = new provider.AdapterProvision(credentials)
 
+    blankLine()
+    blankLine()
+    info('Provision DB Instance')
     await provisionDb(dbProvision, engine, configWriter)
+
+    blankLine()
+    blankLine()
+    info('Provision Adapter')
     await provisionAdapter(adapterProvision, engine)
 }
 
