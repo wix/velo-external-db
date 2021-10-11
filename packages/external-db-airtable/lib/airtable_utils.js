3a8a7c24e541643ea047e1bb93a6c796a9a83da3
Ido <idokah@wix.com>
2021-10-11 19:50:52 +0300

    change minifyRecord

--- packages/external-db-airtable/lib/airtable_utils.js
+++ packages/external-db-airtable/lib/airtable_utils.js
@@ -1,7 +1,7 @@
 const minifyRecord = (record) => {
     const fields = record.fields
-    fields._createdDate = { $date: fields._createdDate }
-    fields._updatedDate = { $date: fields._updatedDate }
+    if (fields._createdDate && typeof fields._createdDate === 'string') fields._createdDate = new Date(fields._createdDate)
+    if (fields._updatedDate && typeof fields._updatedDate === 'string') fields._updatedDate = new Date(fields._updatedDate)
     return fields
 };
 module.exports = { minifyRecord }
