a6f769b0ea95ab51b1f01961e7885fa79d644cff
Noam Almog <noama@wix.com>
2021-10-18 11:35:17 +0300

    renamed files

--- scripts/lib/aws/index.js
+++ scripts/lib/aws/index.js
@@ -1,6 +1,6 @@
-const DbProvision = require('../aws/mysql_aws_installer')
+const DbProvision = require('../aws/db_aws_provision')
 const ConfigWriter = require('../aws/aws_config_writer')
-const AdapterProvision = require('../aws/app_runner_installer')
+const AdapterProvision = require('../aws/adapter_aws_provision')
 
 
 module.exports = { DbProvision, ConfigWriter, AdapterProvision }
