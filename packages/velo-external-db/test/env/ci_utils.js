f76e3559c4c1765b0e6a356087f561797b44c576
Noam Almog <noama@wix.com>
2021-10-19 11:57:58 +0300

    fix local dev check

--- packages/velo-external-db/test/env/ci_utils.js
+++ packages/velo-external-db/test/env/ci_utils.js
@@ -1,14 +1,4 @@
 
-const LocalDev = () => {
-    const zz = process.env.CI
-    console.log(typeof process.env.CI)
-    console.log(typeof zz)
-    console.log(process.env.CI)
-    console.log(zz)
-    console.log(zz === true)
-    console.log(zz === 'true')
-
-    return process.env.CI !== 'true'
-}
+const LocalDev = () => process.env.CI === 'false'
 
 module.exports = { LocalDev }
