18a739dfa060f943aad2003f54feb9f20a5bf730
Max Polsky <maxim.polski@gmail.com>
2021-10-27 14:48:43 +0300

    db-provision refactor

--- scripts/lib/gcp/db_gcp_provision.js
+++ scripts/lib/gcp/db_gcp_provision.js
@@ -9,7 +9,6 @@ class DbProvision {
             credentials : { client_email: gcpClientEmail, private_key: gcpPrivateKey },
             scopes: 'https://www.googleapis.com/auth/cloud-platform'
         })
-        this.instancesApiURL = ( projectId ) => `https://sqladmin.googleapis.com/sql/v1beta4/projects/${projectId}/instances`
     }
 
     asGcp(engine) {
@@ -22,9 +21,9 @@ class DbProvision {
     }
 
     async createDb( { name, engine, credentials }) {
-
         const client = await this.authClient.getClient()
         const projectId = await this.authClient.getProjectId()
+        const apiUrl = `https://sqladmin.googleapis.com/sql/v1beta4/projects/${projectId}/instances`
         const ip = await publicIp.v4()
 
         const dbInstanceProperties = {
@@ -41,28 +40,29 @@ class DbProvision {
             rootPassword: credentials.passwd
         }
 
-        const response = await client.request({ url: this.instancesApiURL( projectId ), method: 'POST', data: dbInstanceProperties})
-
-        return response.operationType === 'CREATE'
+        const response = await client.request({ url: apiUrl, method: 'POST', data: dbInstanceProperties})
+        return response.operationType === 'CREATE' && response.status === 'PENDING'
     }
 
     async dbStatusAvailable(name) {
         const client = await this.authClient.getClient()
         const projectId = await this.authClient.getProjectId()
+        const apiUrl = `https://sqladmin.googleapis.com/sql/v1beta4/projects/${projectId}/instances/${name}`
 
-        const response = await client.request({ url: this.instancesApiURL( projectId ) })
-        const { state, ipAddresses, connectionName, databaseVersion } = response.data.items.find(i => i.name === name)
+        const response = await client.request({ url: apiUrl })
+        const instance = response.data.items.find(i => i.name === name)
 
-        return { 
-            available: state === 'RUNNABLE',
-            ipAddresses: ipAddresses.find(i=> i.type === 'PRIMARY').ipAddress,
-            connectionName, databaseVersion 
+        return {
+            available: instance.state === 'RUNNABLE',
+            host: instance.ipAddresses.find(i => i.type === 'PRIMARY').ipAddress,
+            connectionName: instance.connectionName,
+            databaseVersion: instance.databaseVersion,
         }
     }
 
     async postCreateDb(engine, dbName, status, credentials) {
         await factory.clientFor(engine)
-                     .createDatabase(dbName, status.host, credentials)
+            .createDatabase(dbName, status.host, { ...credentials, user: 'root' })
     }
 }
 
