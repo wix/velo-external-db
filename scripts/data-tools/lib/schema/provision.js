17c37b1c2fcc3cd0122603acded58d3219fdf1ac
Noam Almog <noama@wix.com>
2021-10-31 17:53:21 +0200

    ignore add column error on script

--- scripts/data-tools/lib/schema/provision.js
+++ scripts/data-tools/lib/schema/provision.js
@@ -3,6 +3,8 @@ const create = async (collectionName, axios) => await axios.post('/schemas/creat
 const createCollection = async (collectionName, axios) => await create(collectionName, axios)
 
 const addColumn = (collectionName, column, axios) => axios.post('/schemas/column/add', { collectionName, column })
+                                                          .catch(() => {})
+
 const addColumnsToCollection = async(collectionName, newColumns, axios) => await Promise.all(newColumns.map(column => addColumn(collectionName, column, axios)))
 
 const truncate = (collectionName, axios) => axios.post('/data/truncate', { collectionName })
