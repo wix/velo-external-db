a5aacd62d69cf05c9ded9161f965bf3d1ccebdf6
Ido <idokah@wix.com>
2021-11-01 14:58:38 +0200

    add postgres support and fix typo

--- scripts/provision/lib/cloud-providers/azure/db/factory.js
+++ scripts/provision/lib/cloud-providers/azure/db/factory.js
@@ -2,6 +2,8 @@ const clientFor = engine => {
     switch (engine) {
         case 'mysql':
             return require('./mysql_support')
+        case 'postgres':
+            return require ('./postgres_support')
     }
 }
 
