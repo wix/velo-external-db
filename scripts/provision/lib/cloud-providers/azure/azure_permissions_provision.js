6623b12df18388e84bf09bf6bcd89039da3dcdee
Max Polsky <maxim.polski@gmail.com>
2021-11-11 17:02:48 +0200

    added missing permissionsProvision methods

--- scripts/provision/lib/cloud-providers/azure/azure_permissions_provision.js
+++ scripts/provision/lib/cloud-providers/azure/azure_permissions_provision.js
@@ -2,6 +2,12 @@
 class PermissionsProvision {
     constructor() {
     }
+
+    async createServiceAccount() {
+    }
+
+    async grantPermission() {
+    }
 }
 
 module.exports = PermissionsProvision
