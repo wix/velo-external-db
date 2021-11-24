77b8f542892fce5864c3c2605fa7f3384169ec0e
Noam Almog <noama@wix.com>
2021-11-24 14:12:56 +0200

    added common should not test function for e2e/it

--- packages/test-commons/lib/test-commons.js
+++ packages/test-commons/lib/test-commons.js
@@ -1,5 +1,7 @@
 const Uninitialized = null
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-module.exports = { sleep, Uninitialized }
+const shouldNotRunOn = (impl, current) => !impl.includes(current)
+
+module.exports = { shouldNotRunOn, sleep, Uninitialized }
