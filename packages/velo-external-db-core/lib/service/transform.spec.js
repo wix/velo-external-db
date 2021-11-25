79223dd60c14718022ad64a5b3bfa0ce289c05af
Noam Almog <noama@wix.com>
2021-11-25 16:14:58 +0200

    added schema aware transform helper methods for insert item

--- packages/velo-external-db-core/lib/service/transform.spec.js
+++ packages/velo-external-db-core/lib/service/transform.spec.js
@@ -1,9 +1,13 @@
-const { asWixData, unpackDates, generateIdsIfNeeded } = require('./transform')
+const rewire = require('rewire')
+const transform = rewire('./transform')
+const { asWixData, unpackDates, generateIdsIfNeeded, removeUnneededFields, defaultValueFor, isDate } = transform
 const { Uninitialized, gen } = require('test-commons')
 const Chance = require('chance')
-const chance = Chance();
+const chance = Chance()
+const validate = require('uuid-validate')
+const dateTimeProvider = require('../../test/drivers/date_time_provider_test_support')
 
-describe('Converters', () => {
+fdescribe('Converters', () => {
     test('unpack dates will duplicate object and do nothing is date is not there', async () => {
         expect(unpackDates(ctx.obj)).toEqual(ctx.obj)
     })
@@ -19,9 +23,9 @@ describe('Converters', () => {
     })
 
     test('pack dates will take all properties with date and convert them to velo date', async () => {
-        const objWithJsDates = { ...ctx.obj, [ctx.property]: new Date(ctx.veloDate.$date), [ctx.anotherProperty]: new Date(ctx.veloDate.$date)}
+        const objWithJsDates = { ...ctx.obj, [ctx.property]: dateTimeProvider.currentDateTime(), [ctx.anotherProperty]: dateTimeProvider.currentDateTime()}
 
-        expect(asWixData(objWithJsDates)).toEqual( { ...ctx.obj, [ctx.property]: ctx.veloDate, [ctx.anotherProperty]: ctx.veloDate} )
+        expect(asWixData(objWithJsDates)).toEqual( { ...ctx.obj, [ctx.property]: { $date: dateTimeProvider.currentDateTime().toISOString() }, [ctx.anotherProperty]: { $date: dateTimeProvider.currentDateTime().toISOString() }} )
     })
 
     test('if _id field exists do nothing', async () => {
@@ -29,7 +33,55 @@ describe('Converters', () => {
     })
 
     test('if _id field does not exist generate id', async () => {
-        expect(generateIdsIfNeeded({ ...ctx.obj })).toHaveProperty( '_id' )
+        expect(generateIdsIfNeeded(ctx.obj)).toHaveProperty( '_id' )
+    })
+
+    test('if item has all fields like in schema fields, item should stay the same', async () => {
+        expect(removeUnneededFields(ctx.obj, Object.keys(ctx.obj).map(f => ({ name: f, type: 'text' })))).toEqual( ctx.obj )
+    })
+
+    test('if item contain properties that does not exists in the schema, remove it', async () => {
+        expect(removeUnneededFields({ ...ctx.obj, [ctx.property]: chance.word() }, Object.keys(ctx.obj).map(f => ({ name: f, type: 'text' })))).toEqual( ctx.obj )
+    })
+
+    test('if item does not contain properties that exists in the schema, add default value for them', async () => {
+        expect(removeUnneededFields({ }, [{ name: ctx.property, type: 'text' }] )).toEqual({ [ctx.property]: '' } )
+    })
+
+    test('default value for non primary key text field is empty string', async () => {
+        expect(defaultValueFor( { type: 'text' } )).toEqual( '' )
+        expect(defaultValueFor( { type: 'text', isPrimary: false } )).toEqual( '' )
+    })
+
+    test('default value for primary key text field is a random uuid v4', async () => {
+        expect(validate(defaultValueFor( { type: 'text', isPrimary: true } ), 4)).toBeTruthy()
+    })
+
+    test('default value boolean field is false', async () => {
+        expect(defaultValueFor( { type: 'boolean' } )).toBeFalsy()
+    })
+
+    test('default value number int field 0', async () => {
+        expect(defaultValueFor( { type: 'number', subtype: 'int' } )).toEqual(0)
+        expect(defaultValueFor( { type: 'number', subtype: 'bigint' } )).toEqual(0)
+    })
+
+    test('default value number float field 0.0', async () => {
+        expect(defaultValueFor( { type: 'number', subtype: 'float' } )).toEqual(0.0)
+        expect(defaultValueFor( { type: 'number', subtype: 'double' } )).toEqual(0.0)
+        expect(defaultValueFor( { type: 'number', subtype: 'decimal' } )).toEqual(0.0)
+    })
+
+    test('check date', async () => {
+        expect(isDate( new Date() )).toBeTruthy()
+        expect(isDate( '' )).toBeFalsy()
+        expect(isDate( (new Date()).toISOString() ) ).toBeTruthy()
+    })
+
+    test('default value datetime field is current datetime', async () => {
+        transform.__set__('dateTimeProvider', dateTimeProvider)
+
+        expect(defaultValueFor( { type: 'datetime' } )).toEqual(dateTimeProvider.currentDateTime())
     })
 
     const ctx = {
@@ -40,9 +92,9 @@ describe('Converters', () => {
     };
 
     beforeEach(() => {
-        ctx.obj = gen.randomObject();
-        ctx.property = chance.word();
-        ctx.anotherProperty = chance.word();
-        ctx.veloDate = gen.veloDate();
+        ctx.obj = gen.randomObject()
+        ctx.property = chance.word()
+        ctx.anotherProperty = chance.word()
+        ctx.veloDate = gen.veloDate()
     });
 })
