31adc1669d38cad3d93451703a32b0e751e3bdd9
Max Polsky <maxim.polski@gmail.com>
2021-11-29 12:15:42 +0200

    google-sheet lint refactor

--- packages/external-db-google-sheets/tests/mock_google_sheets_api.js
+++ packages/external-db-google-sheets/tests/mock_google_sheets_api.js
@@ -1,16 +1,15 @@
-/* eslint-disable semi */
-/* eslint-disable no-case-declarations */
+/* eslint-disable no-unused-vars */
 const express = require('express')
 const GoogleSpreadsheetDoc = require('./google_sheet_doc_class')
 
 const app = express()
-const v4SpreadsheetsRouter = express.Router();
+const v4SpreadsheetsRouter = express.Router()
 const doc = new GoogleSpreadsheetDoc('spreadsheet-doc-id', 'spreadsheet-doc-title')
 
 app.use(express.json())
-app.use('/v4/spreadsheets/', v4SpreadsheetsRouter);
+app.use('/v4/spreadsheets/', v4SpreadsheetsRouter)
 
-v4SpreadsheetsRouter.get('/:sheetId/', (_, res) =>{
+v4SpreadsheetsRouter.get('/:sheetId/', (_, res) => {
     res.send(doc.docInfo())
 })
 
@@ -19,7 +18,7 @@ v4SpreadsheetsRouter.get('/:sheetId/values/:range', (req, res) => {
     const [sheetTitle, range] = req.params.range.split('!')
     const sheet = doc.getSheet(sheetTitle.replace(/^'/, '').replace(/'$/, ''))
     if (range.includes('A1:')) {
-        res.send(sheet.getSheetHeader());
+        res.send(sheet.getSheetHeader())
     } else {
         const [_, startIndex, endIndex] = range.match(/[A-Z]+([0-9]+):[A-Z]+([0-9]+)/)
         const getRowsRes = sheet.getRows(parseInt(startIndex), parseInt(endIndex))
@@ -27,14 +26,14 @@ v4SpreadsheetsRouter.get('/:sheetId/values/:range', (req, res) => {
     }
 })
 
-v4SpreadsheetsRouter.post('/:sheetId/:requestType',(req,res) => {
+v4SpreadsheetsRouter.post('/:sheetId/:requestType', (req, res) => {
     let replies 
     switch (req.params.requestType.split(':')[1]) {
         case 'batchUpdate':
             replies = req.body.requests.map(r => batchUpdateFunctions(r, doc) )
-            break;
+            break
     }
-    res.send({updatedSpreadsheet: doc.docInfo(), replies })
+    res.send({ updatedSpreadsheet: doc.docInfo(), replies })
 })
 
 v4SpreadsheetsRouter.post('/:sheetId/values/:sheet_range_requestType', (req, res) => {
@@ -43,7 +42,7 @@ v4SpreadsheetsRouter.post('/:sheetId/values/:sheet_range_requestType', (req, res
     const sheet = doc.getSheet(sheetTitle.replace(/^'/, '').replace(/'$/, ''))
     const addRowsRes = sheet.addRows(req.body.values, range )
     
-    res.send({updates: addRowsRes})
+    res.send({ updates: addRowsRes })
 })
 
 v4SpreadsheetsRouter.put('/:sheetId/values/:requestType', (req, res) => {
@@ -66,16 +65,16 @@ const batchUpdateFunctions = ( request, doc ) => {
     switch (requestType) {
         case 'addSheet':
             const newSheet = doc.addSheet(request.addSheet.properties.title)
-            return {addSheet: newSheet.sheetInfo() }
+            return { addSheet: newSheet.sheetInfo() }
         case 'deleteRange':
-            const {sheetId, startRowIndex, endRowIndex } = request.deleteRange.range
+            const { sheetId, startRowIndex, endRowIndex } = request.deleteRange.range
             const sheet = doc.getSheetById(sheetId)
             return sheet.deleteRows(startRowIndex, endRowIndex)
         default:
-            break;
+            break
     }
 }
 
 
 
-module.exports = { app, cleanupSheets : doc.cleanupSheets }
+module.exports = { app, cleanupSheets: doc.cleanupSheets }
