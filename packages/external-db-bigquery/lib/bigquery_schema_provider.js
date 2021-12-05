b9874d1c3376d997258d86256b81579940df2fc0
Max Polsky <maxim.polski@gmail.com>
2021-12-05 15:43:47 +0200

    log errors

--- packages/external-db-bigquery/lib/bigquery_schema_provider.js
+++ packages/external-db-bigquery/lib/bigquery_schema_provider.js
@@ -11,55 +11,76 @@ class SchemaProvider {
     }
 
     async list() {
-        const res = await this.pool.query('SELECT table_name, column_name AS field, data_type as type, FROM INFORMATION_SCHEMA.COLUMNS')
-        const tables = parseTableData(res[0])
+        try{
+            const res = await this.pool.query('SELECT table_name, column_name AS field, data_type as type, FROM INFORMATION_SCHEMA.COLUMNS')
+            const tables = parseTableData(res[0])
+            return Object.entries(tables)
+                         .map(([collectionName, rs]) => ({
+                             id: collectionName,
+                             fields: rs.map( this.translateDbTypes.bind(this) )
+                            }))
+        } catch(e) {
+            console.log(e)
+        }
 
-        return Object.entries(tables)
-                     .map(([collectionName, rs]) => ({
-                         id: collectionName,
-                         fields: rs.map( this.translateDbTypes.bind(this) )
-                     }))
     }
 
     async create(collectionName, _columns) {
-        const columns = _columns || []
-        const dbColumnsSql = [...SystemFields, ...columns].map( c => this.sqlSchemaTranslator.columnToDbColumnSql(c))
-        await this.pool.createTable(collectionName, { schema: dbColumnsSql })
-                  .catch(translateErrorCodes)         
+        try{
+            const columns = _columns || []
+            const dbColumnsSql = [...SystemFields, ...columns].map( c => this.sqlSchemaTranslator.columnToDbColumnSql(c))
+            await this.pool.createTable(collectionName, { schema: dbColumnsSql })
+                      .catch(translateErrorCodes)         
+        } catch(e) {
+            console.log(e)
+        }
     }
 
     async drop(collectionName) {
-        await this.pool.table(collectionName).delete()
-                  .catch(translateErrorCodes)  
+        try {
+            await this.pool.table(collectionName).delete()
+                      .catch(translateErrorCodes)  
+        } catch(e) {
+            og.error(e)
+        }
     }
 
 
     async addColumn(collectionName, column) {
-        await validateSystemFields(column.name)
-
-        await this.pool.query(`ALTER TABLE ${escapeIdentifier(collectionName)} ADD COLUMN ${escapeIdentifier(column.name)} ${this.sqlSchemaTranslator.dbTypeFor(column)}`)
-                  .catch(translateErrorCodes)
-        
+        try {
+            await validateSystemFields(column.name)
+            await this.pool.query(`ALTER TABLE ${escapeIdentifier(collectionName)} ADD COLUMN ${escapeIdentifier(column.name)} ${this.sqlSchemaTranslator.dbTypeFor(column)}`)
+            .catch(translateErrorCodes)
+        } catch(e) {
+            console.log(e)
+        }        
     }
 
     async removeColumn(collectionName, columnName) {
-        await validateSystemFields(columnName)
+        try {
+            await validateSystemFields(columnName)
+            await this.pool.query(`CREATE OR REPLACE TABLE ${escapeIdentifier(collectionName)} AS SELECT * EXCEPT (${escapeIdentifier(columnName)}) FROM  ${escapeIdentifier(collectionName)}`)
+            .catch(translateErrorCodes)
+        } catch(e) {
+            console.log(e)
+        }
 
-        await this.pool.query(`CREATE OR REPLACE TABLE ${escapeIdentifier(collectionName)} AS SELECT * EXCEPT (${escapeIdentifier(columnName)}) FROM  ${escapeIdentifier(collectionName)}`)
-                         .catch(translateErrorCodes)
+
         
     }
 
     async describeCollection(collectionName) {
-        const res = await this.pool.query(`SELECT table_name, column_name AS field, data_type as type, FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name="${escapeIdentifier(collectionName)}"`)
-
-        if (res[0].length === 0) {
-            throw new CollectionDoesNotExists('Collection does not exists')
+        try{
+            const res = await this.pool.query(`SELECT table_name, column_name AS field, data_type as type, FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name="${escapeIdentifier(collectionName)}"`)
+            if (res[0].length === 0) {
+                throw new CollectionDoesNotExists('Collection does not exists')
+            }
+            return res[0].map( this.translateDbTypes.bind(this) )
+        } catch(e) {
+            console.log(e)
         }
-        return res[0].map( this.translateDbTypes.bind(this) )
     }
-
-
+    
     translateDbTypes(row) {
         row.type = this.sqlSchemaTranslator.translateType(row.type)
         return row
