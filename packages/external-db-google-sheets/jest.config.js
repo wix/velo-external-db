c9bb1a07a1790ef028988fed87ba123ed4bde920
Noam Almog <noama@wix.com>
2021-12-12 14:00:59 +0200

    fixed local tests issues

---
+++ packages/external-db-google-sheets/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    clearMocks: true,
+    verbose: true,
+    roots: ['<rootDir>/lib'],
+    preset: 'ts-jest',
+    testRegex: '(.*\\.spec\\.)js$',
+    testEnvironment: 'node'
+}
