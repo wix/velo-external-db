d16b582a4a6d2792bd6786c5f8474bf81f0b6bd8
Max Polsky <maxim.polski@gmail.com>
2021-12-22 18:42:00 +0200

    feat: airtable - new schemaSupportedOperations function

--- packages/external-db-airtable/lib/index.js
+++ packages/external-db-airtable/lib/index.js
@@ -1,4 +1,4 @@
-const SchemaProvider = require('./airtable_schema_provider')
+const { SchemaProvider, schemaSupportedOperations } = require('./airtable_schema_provider')
 const DataProvider = require('./airtable_data_provider')
 const FilterParser = require('./sql_filter_transformer')
 const SchemaColumnTranslator = require('./sql_schema_translator')
@@ -10,4 +10,5 @@ const DatabaseOperations = require('./airtable_operations')
 const driver = () => require('../tests/drivers/sql_filter_transformer_test_support')
 const opsDriver = () => require('../tests/drivers/db_operations_test_support')
 
-module.exports = { SchemaProvider, DataProvider, FilterParser, SchemaColumnTranslator, driver, init, opsDriver, DatabaseOperations, mockServer }
+module.exports = { SchemaProvider, DataProvider, FilterParser, SchemaColumnTranslator, driver,
+                    init, opsDriver, DatabaseOperations, mockServer, schemaSupportedOperations }
