8fc0fa65aa223809f494235cdedaf84cbb788b9f
Max Polsky <maxim.polski@gmail.com>
2021-12-22 14:32:09 +0200

    test: undo test changes and disable test

--- packages/velo-external-db/test/storage/schema_provider.spec.js
+++ packages/velo-external-db/test/storage/schema_provider.spec.js
@@ -87,13 +87,15 @@ describe('Schema API', () => {
             await expect(env.schemaProvider.addColumn(ctx.collectionName, { name: ctx.columnName, type: 'datetime', subtype: 'timestamp' })).rejects.toThrow(CollectionDoesNotExists)
         })
 
-        test('add column on a an existing collection', async() => {
-            await env.schemaProvider.create(ctx.collectionName, [])
+        if (shouldNotRunOn(['Google-Sheet'], name)) {
+            test('add column on a an existing collection', async() => {
+                await env.schemaProvider.create(ctx.collectionName, [])
 
-            await env.schemaProvider.addColumn(ctx.collectionName, { name: ctx.columnName, type: 'text', subtype: 'text' })
+                await env.schemaProvider.addColumn(ctx.collectionName, { name: ctx.columnName, type: 'datetime', subtype: 'timestamp' })
 
-            await expect( env.schemaProvider.describeCollection(ctx.collectionName) ).resolves.toEqual( hasSameSchemaFieldsLike([{ field: ctx.columnName, type: 'text' }]))
-        })
+                await expect( env.schemaProvider.describeCollection(ctx.collectionName) ).resolves.toEqual( hasSameSchemaFieldsLike([{ field: ctx.columnName, type: 'datetime' }]))
+            })
+        }
 
         if (shouldNotRunOn(['BigQuery'], name)) {
             test('add duplicate column will fail', async() => {
