fce2f6be46cec587b3f4e7b4c46298dfab8c2e0e
Max Polsky <maxim.polski@gmail.com>
2021-12-27 18:46:17 +0200

    feat: create if-operation supported-test function

--- packages/test-commons/lib/test-commons.js
+++ packages/test-commons/lib/test-commons.js
@@ -1,7 +1,14 @@
+const { supportedSchemaOperationsFor } = require('velo-external-db-commons')
+
 const Uninitialized = null
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const shouldNotRunOn = (impl, current) => !impl.includes(current)
 
-module.exports = { shouldNotRunOn, sleep, Uninitialized }
+const testIfOperationsSupportedFor = (impl) => {
+    const supportedOperations = supportedSchemaOperationsFor(impl)
+    return (neededOperations) => neededOperations.every(i => supportedOperations.includes(i)) ? test : test.skip
+}
+
+module.exports = { shouldNotRunOn, sleep, Uninitialized, testIfOperationsSupportedFor }
