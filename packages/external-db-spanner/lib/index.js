4c8f90adf045f256037157ebba05b58f663a944c
Max Polsky <maxim.polski@gmail.com>
2021-12-27 16:47:32 +0200

    refactor: spanner - use supported operations func

--- packages/external-db-spanner/lib/index.js
+++ packages/external-db-spanner/lib/index.js
@@ -1,8 +1,8 @@
-const { SchemaProvider, schemaSupportedOperations } = require('./spanner_schema_provider')
+const SchemaProvider = require('./spanner_schema_provider')
 const DataProvider = require('./spanner_data_provider')
 const init = require('./connection_provider')
 
 const driver = () => require('../tests/drivers/sql_filter_transformer_test_support')
 const opsDriver = () => require('../tests/drivers/db_operations_test_support')
 
-module.exports = { SchemaProvider, init, driver, opsDriver, DataProvider, schemaSupportedOperations }
+module.exports = { SchemaProvider, init, driver, opsDriver, DataProvider }
