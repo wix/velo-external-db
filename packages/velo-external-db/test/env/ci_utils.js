4aaf4f1fcba0510b63cd67979acc6a0e093b8cc6
Max Polsky <maxim.polski@gmail.com>
2022-01-02 17:27:09 +0200

    ci: run env.dv setup and teardown when testing google-sheet

--- packages/velo-external-db/test/env/ci_utils.js
+++ packages/velo-external-db/test/env/ci_utils.js
@@ -3,6 +3,6 @@ const LocalDev = () => process.env.CI === undefined || process.env.CI === 'false
 
 const engineWithoutDocker = (engine) => (enginesWithoutDocker.includes(engine))
 
-const enginesWithoutDocker = ['airtable']
+const enginesWithoutDocker = ['airtable', 'google-sheet']
 
 module.exports = { LocalDev, engineWithoutDocker }
