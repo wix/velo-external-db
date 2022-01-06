5f7e1cdd2c414be5636a3dd211408c058b622fe7
Max Polsky <maxim.polski@gmail.com>
2022-01-06 17:00:15 +0200

    feat: matches for schema test

---
+++ packages/velo-external-db-core/lib/service/schema_matchers.js
@@ -0,0 +1,22 @@
+const { asWixSchema, allowedOperationsFor, prepareFieldsList } = require('velo-external-db-commons')
+
+const schemasList = (dbs, allowedSchemaOperations) => {
+    const dbsList = dbs.map( db => ({
+        ...db,
+        allowedOperations: allowedOperationsFor(db),
+        allowedSchemaOperations,
+        fields: prepareFieldsList(db.fields)
+    }))
+
+    return dbsList.map(asWixSchema)
+}
+
+const readWriteSchemaList = (dbs, allowedSchemaOperations) => {
+    const idColumn = { field: '_id', type: 'text' }
+    const dbsWithIdColumn = dbs.map( i => ({ ...i, fields: [ ...i.fields, idColumn] }) )
+    
+    return schemasList(dbsWithIdColumn, allowedSchemaOperations)
+}
+
+
+module.exports = { schemasList, readWriteSchemaList }
