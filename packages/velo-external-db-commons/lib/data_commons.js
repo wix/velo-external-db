4daaad4862c1c4a974bc6688997e83aaa47171f0
Max Polsky <maxim.polski@gmail.com>
2022-01-13 13:08:53 +0200

    feat: wixOpToAdapterOp function in data-commons

--- packages/velo-external-db-commons/lib/data_commons.js
+++ packages/velo-external-db-commons/lib/data_commons.js
@@ -65,10 +65,44 @@ const AdapterFunctions = {
     count: 'count'
 }
 
+const wixOperatorToAdapterOperator = operator => {
+    switch (operator) {
+        case '$eq':
+            return AdapterOperators.eq
+        case '$ne':
+            return AdapterOperators.ne
+        case '$lt':
+            return AdapterOperators.lt
+        case '$lte':
+            return AdapterOperators.lte
+        case '$gt': 
+            return AdapterOperators.gt
+        case '$gte':
+            return AdapterOperators.gte
+        case '$hasSome':
+            return AdapterOperators.include
+        case '$contains':
+            return AdapterOperators.string_contains
+        case '$startsWith':
+            return AdapterOperators.string_begins
+        case '$endsWith':
+            return AdapterOperators.string_ends
+        case '$or':
+            return AdapterOperators.or
+        case '$and':
+            return AdapterOperators.and
+        case '$not':
+            return AdapterOperators.not
+        case '$urlized':
+            return AdapterOperators.urlized
+    }
+}
+
 const extractGroupByNames = (projection) =>  projection.filter(f => !f.function).map( f => f.name ) 
 
 const extractProjectionFunctionsObjects = (projection) => projection.filter(f => f.function)
 
 module.exports = { EMPTY_FILTER, EMPTY_SORT, patchDateTime, asParamArrays, isObject, updateFieldsFor,
                      isEmptyFilter, AdapterOperators, AdapterFunctions,
-                     extractGroupByNames, extractProjectionFunctionsObjects }
+                     extractGroupByNames, extractProjectionFunctionsObjects,
+                     wixOperatorToAdapterOperator }
--- packages/velo-external-db-core/lib/converters/filter_transformer.js
+++ packages/velo-external-db-core/lib/converters/filter_transformer.js
@@ -1,4 +1,4 @@
-const { AdapterOperators, isObject } = require('velo-external-db-commons')
+const { isObject, wixOperatorToAdapterOperator } = require('velo-external-db-commons')
 const { EMPTY_FILTER } = require ('./utils')
 const { InvalidQuery } = require('velo-external-db-commons').errors
 
@@ -38,40 +38,14 @@ class FilterTransformer {
     }
 
 
-    wixOperatorToAdapterOperator(operator) {
-        switch (operator) {
-            case '$eq':
-                return AdapterOperators.eq
-            case '$ne':
-                return AdapterOperators.ne
-            case '$lt':
-                return AdapterOperators.lt
-            case '$lte':
-                return AdapterOperators.lte
-            case '$gt': 
-                return AdapterOperators.gt
-            case '$gte':
-                return AdapterOperators.gte
-            case '$hasSome':
-                return AdapterOperators.include
-            case '$contains':
-                return AdapterOperators.string_contains
-            case '$startsWith':
-                return AdapterOperators.string_begins
-            case '$endsWith':
-                return AdapterOperators.string_ends
-            case '$or':
-                return AdapterOperators.or
-            case '$and':
-                return AdapterOperators.and
-            case '$not':
-                return AdapterOperators.not
-            case '$urlized':
-                return AdapterOperators.urlized
-                
-            default:
-                throw new InvalidQuery(`Unrecognized operator ${operator}`)
+    wixOperatorToAdapterOperator(wixOperator) {
+        const adapterOperator = wixOperatorToAdapterOperator(wixOperator)
+
+        if( adapterOperator === undefined) {
+            throw new InvalidQuery(`Unrecognized operator ${wixOperator}`)
         }
+
+        return adapterOperator        
     }
 
     isEmptyFilter(filter) {
