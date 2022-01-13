09bef16896136cbf05ec223755e29d468c776ff0
Max Polsky <maxim.polski@gmail.com>
2022-01-13 18:25:33 +0200

    fix: row with null values will not throw

--- packages/external-db-bigquery/lib/bigquery_utils.js
+++ packages/external-db-bigquery/lib/bigquery_utils.js
@@ -1,11 +1,9 @@
-const { SchemaOperations } = require('velo-external-db-commons')
-const { LIST, LIST_HEADERS, CREATE, DROP, ADD_COLUMN, REMOVE_COLUMN, DESCRIBE_COLLECTION } = SchemaOperations
-
-const schemaSupportedOperations =  [LIST, LIST_HEADERS, CREATE, DROP, ADD_COLUMN, REMOVE_COLUMN, DESCRIBE_COLLECTION]
 
 // Ported from PostgreSQL 9.2.4 source code in src/interfaces/libpq/fe-exec.c
 const escapeIdentifier = (str) => `\`${(str || '').replace(/"/g, '""')}\``
 const wildCardWith = (n, char) => Array(n).fill(char, 0, n).join(', ')
+
+const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/
 
 const patchDateTime = (item) => {
     const obj = {}
@@ -21,22 +19,25 @@ const patchDateTime = (item) => {
     return obj
 }
 
-
 const unPatchDateTime = (item) => {
     const obj = {}
     for (const key of Object.keys(item)) {
-        const value = item[key].value
-        const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/
+        const value = item[key]?.value
 
-        if (reISO.test(value)) {
+        if (isDate(value)) {
             obj[key] = new Date(value)
-        } else {
-            obj[key] = item[key].toNumber ? item[key].toNumber() : item[key]
         }
+        else if (isNumber(item[key])) {
+            obj[key] = item[key].toNumber()
+        }
+        else {
+            obj[key] = item[key]
+        } 
     }
-
     return obj
 }
 
+const isNumber = (value) => value !== null && value.toNumber
+const isDate = (value) => reISO.test(value)
 
-module.exports = { patchDateTime, unPatchDateTime, escapeIdentifier, wildCardWith, schemaSupportedOperations }
+module.exports = { patchDateTime, unPatchDateTime, escapeIdentifier, wildCardWith }
