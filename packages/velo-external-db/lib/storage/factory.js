bd7b26c0f6680a556f90c7245e127f50cad18f61
Ido <idokah@wix.com>
2022-01-17 15:37:40 +0200

    remove read config duplicate

--- packages/velo-external-db/lib/storage/factory.js
+++ packages/velo-external-db/lib/storage/factory.js
@@ -2,62 +2,54 @@ const append = (res, secretKey) => ( { ...res, secretKey: secretKey } )
 
 const init = async(type, vendor, config) => {
     console.log(`INIT: ${vendor + '/' + type}`)
+    const cfg = await config.readConfig()
     switch ( type.toLowerCase() ) {
         case 'postgres': {
             const { init } = require('external-db-postgres')
-            const cfg = await config.readConfig()
 
             return append(init(cfg), cfg.secretKey)
         }
         case 'spanner': {
             const { init } = require('external-db-spanner')
 
-            const cfg = await config.readConfig()
             return append(init(cfg), cfg.secretKey)
         }
         case 'firestore': {
             const { init } = require('external-db-firestore')
 
-            const { projectId, secretKey } = await config.readConfig()
+            const { projectId, secretKey } = cfg
             return append(init([projectId]), secretKey)
         }
         case 'mssql': {
             const { init } = require('external-db-mssql')
 
-            const cfg = await config.readConfig()
             const res = await init(cfg)
             return append(res, cfg.secretKey)
         }
         case 'mysql': {
             const { init } = require('external-db-mysql')
-            const cfg = await config.readConfig()
 
             return append(init(cfg), cfg.secretKey)
         }
         case 'mongo': {
             const { init } = require('external-db-mongo')
-            const cfg = await config.readConfig()
 
             return append(await init(cfg), cfg.secretKey)
         }
         case 'google-sheet': {
             const { init } = require('external-db-google-sheets')
-            const cfg = await config.readConfig()
             return append(await init(cfg), cfg.secretKey)
         }
         case 'airtable': {
             const { init } = require('external-db-airtable')
-            const cfg = await config.readConfig()
             return append(await init(cfg), cfg.secretKey)
         }
         case 'dynamodb': {
             const { init } = require('external-db-dynamodb')
-            const cfg = await config.readConfig()
             return append(await init(cfg), cfg.secretKey)
         }
         case 'bigquery': {
             const { init } = require('external-db-bigquery')
-            const cfg = await config.readConfig()
             return append(await init(cfg), cfg.secretKey)
         }
     }
