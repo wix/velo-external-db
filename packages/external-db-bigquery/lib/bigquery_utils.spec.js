229aa2b4231c6c34ef024770a59621a7c998de5c
Max Polsky <maxim.polski@gmail.com>
2022-01-18 18:19:58 +0200

    test: bigquery test refactor

--- packages/external-db-bigquery/lib/bigquery_utils.spec.js
+++ packages/external-db-bigquery/lib/bigquery_utils.spec.js
@@ -1,26 +1,52 @@
-const { when } = require('jest-when')
+const { Uninitialized } = require('test-commons')
 const { unPatchDateTime } = require('./bigquery_utils')
+const driver = require('./bigquery_utils_test_support')
+const Chance = require('chance')
+const chance = Chance()
 
 describe('BigQuery Utils', () => {
-    describe('unPatchDateTime', () => {
-        test('Object with null value should be returned untouched', () => {
-            const objectWithNullValue = { a: null }
-        
-            const actual = unPatchDateTime(objectWithNullValue)
+    describe('unPatchDateTime Function', () => {
+        test('Object with null value should be returned with null value', () => {        
+            driver.givenNullValueTo(ctx.propertyName)
 
-            expect(actual).toEqual(objectWithNullValue)
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: null })
         })
 
-        test('Object with number value should be contain number', () => {
-            const bigQueryNumber = {
-                toNumber: jest.fn(),
-            }           
-            when(bigQueryNumber.toNumber).calledWith().mockReturnValue(42) 
-            const objectWithNumber = { a: bigQueryNumber }
-            
-            const actual = unPatchDateTime(objectWithNumber)
-
-            expect(actual).toEqual({ a: 42 })
+        test('Object with number value should contain number', () => {         
+            driver.givenNumberValueTo(ctx.propertyName, ctx.randomNumber)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: ctx.randomNumber })
         })
+
+        test('Object with date value should contain date object', () => {         
+            driver.givenDateValueTo(ctx.propertyName, ctx.randomDate)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: ctx.randomDate })
+        })
+
+        test('Object with wrong date value should not be convert to date object', () => {         
+            driver.givenWrongFormatDateValueTo(ctx.propertyName, ctx.randomDate)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: (ctx.randomDate).toISOString() })
+        })
+
+    })
+
+    const ctx = {
+        propertyName: Uninitialized,
+        randomNumber: Uninitialized,
+        randomDate: Uninitialized,
+    }
+
+    const env = {
+        bigQueryItem: Uninitialized,
+    }
+
+    beforeEach(() => {
+        ctx.propertyName = chance.word()
+        ctx.randomNumber = chance.natural()
+        ctx.randomDate = new Date(chance.date())
+        driver.reset()
+        env.bigQueryItem = driver.bigQueryItem
     })
 })
