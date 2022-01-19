ab6235a23c2eb557a1dd52cc4a30417b80f336f0
Max Polsky <maxim.polski@gmail.com>
2022-01-19 17:02:56 +0200

    Bigquery bug fix  (#145)
    * fix: row with null values will not throw
    
    * test: new test for bigquery utils
    
    * test: bigquery test refactor
    
    * style:  update test description
    
    * style: minor style refactor

---
+++ packages/external-db-bigquery/lib/bigquery_utils.spec.js
@@ -0,0 +1,52 @@
+const { Uninitialized } = require('test-commons')
+const { unPatchDateTime } = require('./bigquery_utils')
+const driver = require('./bigquery_utils_test_support')
+const Chance = require('chance')
+const chance = Chance()
+
+describe('BigQuery Utils', () => {
+    describe('unPatchDateTime Function', () => {
+        test('Object with a null value should contain a null field', () => {        
+            driver.givenNullValueTo(ctx.propertyName)
+
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: null })
+        })
+
+        test('Object with a number value should contain a number field', () => {         
+            driver.givenNumberValueTo(ctx.propertyName, ctx.randomNumber)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: ctx.randomNumber })
+        })
+
+        test('Object with a date object value should contain a date object', () => {         
+            driver.givenDateValueTo(ctx.propertyName, ctx.randomDate)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: ctx.randomDate })
+        })
+
+        test('Object with a date string value should not be converted to a date object', () => {         
+            driver.givenWrongFormatDateValueTo(ctx.propertyName, ctx.randomDate)
+                        
+            expect( unPatchDateTime(env.bigQueryItem) ).toEqual({ [ctx.propertyName]: (ctx.randomDate).toISOString() })
+        })
+
+    })
+
+    const ctx = {
+        propertyName: Uninitialized,
+        randomNumber: Uninitialized,
+        randomDate: Uninitialized,
+    }
+
+    const env = {
+        bigQueryItem: Uninitialized,
+    }
+
+    beforeEach(() => {
+        ctx.propertyName = chance.word()
+        ctx.randomNumber = chance.natural()
+        ctx.randomDate = new Date(chance.date())
+        driver.reset()
+        env.bigQueryItem = driver.bigQueryItem
+    })
+})
