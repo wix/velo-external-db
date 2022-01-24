0acd4dd153a4fe443343d052f663e37e40d4bb4a
Max Polsky <maxim.polski@gmail.com>
2022-01-24 16:19:48 +0200

    feat: return local auth when configStatus is broken

--- packages/external-db-authorization/lib/factory.js
+++ packages/external-db-authorization/lib/factory.js
@@ -1,7 +1,4 @@
-
-const createAuthService = async(vendor, config) => {
-  const cfg = await config.readConfig()
-
+const strategyFor = (vendor = '', cfg) => {
   switch (vendor.toLowerCase()) {
     case 'aws': {
       const { AwsStrategy } = require('./strategies/aws_strategy')
@@ -16,12 +13,24 @@ const createAuthService = async(vendor, config) => {
     }
 
     case 'azure':
+    case 'local':
     default: {
       const { LocalStrategy } = require('./strategies/local_strategy')
       
       return new LocalStrategy(cfg)
     }      
   }
+}
+
+const createAuthService = async(vendor, config) => {
+  const cfg = await config.readConfig()
+  const configStatus = await config.configStatus()
+
+  if (configStatus !== 'External DB Config read successfully') {
+    return strategyFor('local')
+  }
+  
+  return strategyFor(vendor, cfg)
 
 }
 
