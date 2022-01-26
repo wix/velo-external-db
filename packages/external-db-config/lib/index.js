f82925a1d2c54d7ebf237c6f90d9505055de15b0
Max Polsky <maxim.polski@gmail.com>
2022-01-26 20:25:33 +0200

    refactor: removed auth_factory

--- packages/external-db-config/lib/index.js
+++ packages/external-db-config/lib/index.js
@@ -1,8 +1,7 @@
 const { create } = require('./factory')
-const { createAuthConfigReader } = require('./auth_factory')
 const CommonConfigReader = require('./readers/common_config_reader')
 
 
 const readCommonConfig = () => new CommonConfigReader().readConfig()
 
-module.exports = { create, readCommonConfig, createAuthConfigReader }
+module.exports = { create, readCommonConfig }
