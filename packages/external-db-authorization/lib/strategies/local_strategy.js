12ae2487615374d9d79921bf4eb42f45d54c791b
Max Polsky <maxim.polski@gmail.com>
2022-02-02 19:25:07 +0200

    init of genral-authentication

--- packages/external-db-authorization/lib/strategies/local_strategy.js
+++ packages/external-db-authorization/lib/strategies/local_strategy.js
@@ -1,10 +1,19 @@
-const localStrategy = require('passport-local')
-
+const OAuth2Strategy = require('passport-oauth2')
 class LocalStrategy {
-  constructor() {
-    return new localStrategy(this.verify) 
+  constructor({ clientDomain, clientId, clientSecret, callbackUrl, passReqToCallback }) {
+    this.options = {
+      authorizationURL: `${clientDomain}/authorize`,
+      tokenURL: `${clientDomain}/token`,
+      callbackURL: callbackUrl,
+      clientID: clientId,
+      clientSecret,
+      passReqToCallback,
+      scope: [''],
+      state: [],
+  } 
+    return new OAuth2Strategy(this.options, this.verify)
   }
-
+  
   verify(AccessToken, tokenSecret, profile, done) {
         done(null, profile)
   }
