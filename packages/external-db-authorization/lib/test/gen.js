d3fcb97764d9acc745ac823bc530cbceaa6f731c
Idokah <82806105+Idokah@users.noreply.github.com>
2022-02-03 12:01:07 +0200

    Move role authorization to module (#168)
    * move role-authorization to module and create gen.js for this module
    
    * jest config, deps

---
+++ packages/external-db-authorization/lib/test/gen.js
@@ -0,0 +1,33 @@
+const { gen }  = require('test-commons')
+const Chance = require('chance')
+const chance = Chance()
+
+const veloRoles = ['OWNER', 'BACKEND_CODE', 'MEMBER', 'VISITOR']
+
+const collectionConfigEntity = () => ({
+    id: chance.word(),
+    readPolicies: gen.randomElementsFromArray(veloRoles),
+    writePolicies: gen.randomElementsFromArray(veloRoles)
+})
+
+const authorizationConfig = () => gen.randomArrayOf( collectionConfigEntity )
+
+const collectionNameFrom = (config) => gen.randomObjectFromArray(config).id
+
+const readRolesFor = (collectionName, config) => config.find(c => c.id = collectionName).readPolicies
+const writeRolesFor = (collectionName, config) => config.find(c => c.id = collectionName).writePolicies
+
+const authorizedReadRoleFor = (collectionName, config) => gen.randomObjectFromArray(readRolesFor(collectionName, config))
+
+const unauthorizedReadRoleFor = (collectionName, config) => gen.randomObjectFromArray(
+                                                                veloRoles.filter(x => !readRolesFor(collectionName, config).includes(x))
+                                                                )
+
+const authorizedWriteRoleFor = (collectionName, config) => gen.randomObjectFromArray(writeRolesFor(collectionName, config))
+
+const unauthorizedWriteRoleFor = (collectionName, config) => gen.randomObjectFromArray(
+                                                                veloRoles.filter(x => !writeRolesFor(collectionName, config).includes(x))
+                                                                )
+
+
+module.exports = { authorizationConfig, collectionNameFrom, authorizedReadRoleFor, unauthorizedReadRoleFor, authorizedWriteRoleFor, unauthorizedWriteRoleFor }
