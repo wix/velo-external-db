34e3a26730ab062f302f1bcb4150294c9acad429
Max Polsky <maxim.polski@gmail.com>
2022-02-06 14:38:49 +0200

    refactor: extracted function

--- packages/external-db-authorization/lib/auth-providers/gcp_auth_provider.js
+++ packages/external-db-authorization/lib/auth-providers/gcp_auth_provider.js
@@ -10,31 +10,31 @@ class GcpAuthProvider {
       scope: ['email', 'profile']
     }
 
-    return new GoogleStrategy(this.options, this.verify)
+    return new GoogleStrategy(this.options, this.verify.bind(this))
   }
 
   verify(accessToken, tokenSecret, profile, cb) {
-    const usersEmail = profile._json.email
+    const usersEmail = profile._json.email 
 
-    const getProjectOwners = async() => {
-      const authClient = new GoogleAuth({ scopes: 'https://www.googleapis.com/auth/cloud-platform' })
-      const client = await authClient.getClient()
-      const projectId = await authClient.getProjectId()
-      const getIamPolicyRestUrl = `https://cloudresourcemanager.googleapis.com/v1/projects/${projectId}:getIamPolicy`
-  
-      const res = await client.request({ method: 'POST', url: getIamPolicyRestUrl })
-      const { members: allOwners } = res.data.bindings.find(i => i.role === 'roles/owner')
-  
-      const projectOwnersUsers = allOwners.filter(i => i.startsWith('user:')).map(i => i.split(':')[1])
-
-      return projectOwnersUsers
-    }
-
-    getProjectOwners()
-            .then(projectOwners => projectOwners.includes(usersEmail) ? cb(null, profile) : cb('Unauthorized', null))
-            .catch(err => cb(`Error fetching project owners: ${err}`, null) )
+    this.getProjectOwners().then(projectOwners => projectOwners.includes(usersEmail) ? cb(null, profile) : cb('Unauthorized', null))
+                           .catch(err => cb(`Error fetching project owners: ${err}`, null) )
   
   }
+
+  async getProjectOwners() {
+    const authClient = new GoogleAuth({ scopes: 'https://www.googleapis.com/auth/cloud-platform' })
+    const client = await authClient.getClient()
+    const projectId = await authClient.getProjectId()
+    const getIamPolicyRestUrl = `https://cloudresourcemanager.googleapis.com/v1/projects/${projectId}:getIamPolicy`
+
+    const res = await client.request({ method: 'POST', url: getIamPolicyRestUrl })
+    const { members: allOwners } = res.data.bindings.find(i => i.role === 'roles/owner')
+
+    const projectOwnersUsers = allOwners.filter(i => i.startsWith('user:')).map(i => i.split(':')[1])
+
+    return projectOwnersUsers
+  }
+
 
 }
 
