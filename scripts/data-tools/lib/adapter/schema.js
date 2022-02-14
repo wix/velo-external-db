86166af98d763ad13849fb11da1e88822287c20d
Ido <idokah@wix.com>
2022-02-14 14:56:53 +0200

    fix add columns id data script

--- scripts/data-tools/lib/adapter/schema.js
+++ scripts/data-tools/lib/adapter/schema.js
@@ -2,10 +2,17 @@
 const create = async(collectionName, axios) => await axios.post('/schemas/create', { collectionName })
 const createCollection = async(collectionName, axios) => await create(collectionName, axios)
 
-const addColumn = (collectionName, column, axios) => axios.post('/schemas/column/add', { collectionName, column })
-                                                          .catch(() => {})
+const addColumn = async(collectionName, column, axios) => {
+    try{
+        return await axios.post('/schemas/column/add', { collectionName, column })
+    }catch(e) {console.log(e)}
+}
 
-const addColumnsToCollection = async(collectionName, newColumns, axios) => await Promise.all(newColumns.map(column => addColumn(collectionName, column, axios)))
+const addColumnsToCollection = async(collectionName, newColumns, axios) => {
+    for (const c of newColumns) {
+        await addColumn(collectionName, c, axios)
+    }
+}
 
 const truncate = (collectionName, axios) => axios.post('/data/truncate', { collectionName })
 
