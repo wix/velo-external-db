096ffc52e2e6f505c90d4c6f722f6e7501826eb5
Ido <idokah@wix.com>
2022-02-16 19:56:48 +0200

    auth config reader validator

--- packages/external-db-config/lib/readers/authorization_config_reader.js
+++ packages/external-db-config/lib/readers/authorization_config_reader.js
@@ -1,18 +1,27 @@
-const { checkRequiredKeys, isJson, EMPTY_ROLE_CONFIG } = require('../utils/config_utils')
+const { checkRequiredKeys, isJson, EMPTY_ROLE_CONFIG, configPattern, collectionConfigPattern } = require('../utils/config_utils')
+const Avj = require('ajv')
+const ajv = new Avj()
 
 class AuthorizationConfigReader {
   constructor() {
+    this.configValidator = ajv.compile(configPattern)
+    this.collectionValidator = ajv.compile(collectionConfigPattern)
   }
 
   async readConfig() {
     const { ROLE_CONFIG } = process.env
     const { collectionLevelConfig } = isJson(ROLE_CONFIG) ? JSON.parse(ROLE_CONFIG) : EMPTY_ROLE_CONFIG
-    return collectionLevelConfig
+
+    return collectionLevelConfig.filter(collection => this.collectionValidator(collection))
   }
 
   validate() {
+    const { ROLE_CONFIG } = process.env
+
     return {
-      missingRequiredSecretsKeys: checkRequiredKeys(process.env, [])
+      valid: isJson(ROLE_CONFIG) && this.configValidator(JSON.parse(ROLE_CONFIG)),
+      message: this.configValidator.errors ? this.configValidator.errors.map(e => e.message).join(', ') : 'Authorization config is valid',
+      missingRequiredSecretsKeys: checkRequiredKeys(process.env, ['ROLE_CONFIG'])
     }
   }
 }
--- packages/external-db-config/lib/utils/config_utils.js
+++ packages/external-db-config/lib/utils/config_utils.js
@@ -6,10 +6,30 @@ const supportedDBs = ['postgres', 'spanner', 'firestore', 'mssql', 'mysql', 'mon
 
 const supportedVendors = ['gcp', 'aws', 'azure']
 
+const veloRoles = ['OWNER', 'BACKEND_CODE', 'VISITOR', 'MEMBER']
+
+const collectionConfigPattern = {
+    type: 'object',
+    properties: {
+        id: { type: 'string' },
+        readPolicies: { type: 'array', items: { enum: veloRoles } },
+        writePolicies: { type: 'array', items: { enum: veloRoles } },
+    },
+    required: ['id']
+}
+
+const configPattern = {
+    type: 'object',
+    properties: {
+        collectionLevelConfig: { items: collectionConfigPattern }
+    },
+    required: ['collectionLevelConfig']
+}
+
 const isJson = (str) => { try { JSON.parse(str); return true } catch (e) { return false } }
 
 const EMPTY_ROLE_CONFIG = {
-    collectionLevelRoleConfig: []
+    collectionLevelConfig: []
 }
 
-module.exports = { checkRequiredKeys, supportedDBs, supportedVendors, isJson, EMPTY_ROLE_CONFIG }
+module.exports = { checkRequiredKeys, supportedDBs, supportedVendors, isJson, EMPTY_ROLE_CONFIG, configPattern, collectionConfigPattern }
