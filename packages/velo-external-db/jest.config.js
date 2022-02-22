eaf089a439707b49838ddc9dc7695e3423e9d670
Noam Almog <noama@wix.com>
2022-02-22 05:09:21 -0800

    updated deps (#199)
    removed ts preset for jest

--- packages/velo-external-db/jest.config.js
+++ packages/velo-external-db/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     clearMocks: true,
     verbose: true,
     roots: ['<rootDir>/lib', '<rootDir>/test'],
-    preset: 'ts-jest',
     testRegex: '(.*\\.spec\\.)js$',
     testEnvironment: 'node',
     globalSetup: './test/env/env.db.setup.js',
