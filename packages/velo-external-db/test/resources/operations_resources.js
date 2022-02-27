73fdbc8ee60f037b70cc63344f8917711493c620
michaelir <michaelir@wix.com>
2022-02-27 14:42:02 +0200

    fixed typo

--- packages/velo-external-db/test/resources/operations_resources.js
+++ packages/velo-external-db/test/resources/operations_resources.js
@@ -42,7 +42,7 @@ const testSuits = {
 }
 
 const testedSuit = () => testSuits[process.env.TEST_ENGINE]
-const setupDb = () => testedSuit().setup
+const setupDb = () => testedSuit().setup()
 const currentDbImplementationName = () => testedSuit().name
 
 module.exports = { env, setupDb, currentDbImplementationName }
