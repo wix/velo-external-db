55ff697610429b3f5bb2c6cba593c1846a84b21e
Max Polsky <maxim.polski@gmail.com>
2022-03-02 13:34:19 +0200

    feat:  generate a number of collections input

--- scripts/data-tools/lib/core/process.js
+++ scripts/data-tools/lib/core/process.js
@@ -7,23 +7,30 @@ const gen = require('../generator/schema')
 const main = async(userInputs) => {
     const axios = axiosFor(userInputs)
 
-    info('Creating schema on target adapter')
-    await startSpinnerWith('Creating new collection', async() => await schema.createCollection(userInputs.collectionName, axios), 'Collection was created successfully')
+    for (let i = 0; i < userInputs.numberOfCollection; i++) {
+        
+        const collectionName = `${userInputs.collectionName}_${i}`   
+        info(`Creating collection ${i + 1}/${userInputs.numberOfCollection}: ${collectionName}`)
 
-    const extraColumns = gen.generateColumns(userInputs.columnCount)
-    await startSpinnerWith('Adding new columns to the collection', async() => await schema.addColumnsToCollection(userInputs.collectionName, extraColumns, axios), 'The columns were added successfully')
+        info('Creating schema on target adapter')
+        await startSpinnerWith('Creating new collection', async() => await schema.createCollection(collectionName, axios), 'Collection was created successfully')
+        
+        const extraColumns = gen.generateColumns(userInputs.columnCount)
+        await startSpinnerWith('Adding new columns to the collection', async() => await schema.addColumnsToCollection(collectionName, extraColumns, axios), 'The columns were added successfully')
 
-    if (userInputs.truncate) {
-        await startSpinnerWith('Truncating collection', async() => await schema.truncate(userInputs.collectionName, axios), 'Collection truncated successfully')
+        if (userInputs.truncate) {
+            await startSpinnerWith('Truncating collection', async() => await schema.truncate(collectionName, axios), 'Collection truncated successfully')
+        }
+        info('Loading sample data')
+        const chunkSize = 100
+        await startProgress('progress', userInputs.rowCount / chunkSize, async() => await data.insertChunk(chunkSize, extraColumns, collectionName, axios))
+
+        blankLine()
+        blankLine()
+
     }
-
-    blankLine()
-    blankLine()
-    info('Loading sample data')
-    const chunkSize = 100
-    await startProgress('progress', userInputs.rowCount / chunkSize, async() => await data.insertChunk(chunkSize, extraColumns, userInputs.collectionName, axios))
-
 
 }
 
 module.exports = { main }
+
