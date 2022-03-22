54f99de8f073197d2d1ab06cd909c7651f1e18d6
Ido <idokah@wix.com>
2022-03-22 17:26:45 +0200

    resolve only after downloading the file

--- packages/external-db-mongo/lib/documentdb_utils.js
+++ packages/external-db-mongo/lib/documentdb_utils.js
@@ -3,18 +3,23 @@ const https = require('https')
 const fs = require('fs')
 
 const documentDBConnectionOptions = async(cfg, path) => {
-    const documentDbUri = new URL(cfg.connectionUri)
+    return new Promise(resolve => {
+        const documentDbUri = new URL(cfg.connectionUri)
 
-    if (documentDbUri.searchParams.get('ssl_ca_certs') === 'rds-combined-ca-bundle.pem') {
-        documentDbUri.searchParams.delete('ssl_ca_certs')
-        await https.get('https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem', resp => resp.pipe(fs.createWriteStream(`${path}/rds-combined-ca-bundle.pem`)))
-        return {
-            connectionUri: documentDbUri.toString(),
-            options: { tlsCAFile: `${path}/rds-combined-ca-bundle.pem` }
+        if (documentDbUri.searchParams.get('ssl_ca_certs') === 'rds-combined-ca-bundle.pem') {
+            documentDbUri.searchParams.delete('ssl_ca_certs')
+            const file = fs.createWriteStream(`${path}/rds-combined-ca-bundle.pem`)
+            https.get('https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem', resp => resp.pipe(file))
+            file.on('finish', () => resolve({
+                connectionUri: documentDbUri.toString(), options: { tlsCAFile: `${path}/rds-combined-ca-bundle.pem` }
+            }))
         }
-    }
-    
-    return { connectionUri: documentDbUri.toString(), options: {} }
+
+        else {
+            resolve( { connectionUri: documentDbUri.toString(), options: {} })
+        }
+    })
+
 }
 
 module.exports = { documentDBConnectionOptions }
