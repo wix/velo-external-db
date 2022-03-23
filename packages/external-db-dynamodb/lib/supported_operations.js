15b9e120c7402fa0a419fc8fa89221d35fd1e239
Max Polsky <maxim.polski@gmail.com>
2022-03-23 13:41:50 +0200

    feat: new schema operation to dynamodb

--- packages/external-db-dynamodb/lib/supported_operations.js
+++ packages/external-db-dynamodb/lib/supported_operations.js
@@ -1,7 +1,5 @@
-const { SchemaOperations } = require('velo-external-db-commons')
+const { List, ListHeaders, Create, Drop, AddColumn, RemoveColumn, Describe, BulkDelete, Truncate, DeleteImmediately, UpdateImmediately, Projection, StartWithCaseSensitive } = require('velo-external-db-commons').SchemaOperations
 
-const { List, ListHeaders, Create, Drop, AddColumn, RemoveColumn, Describe, BulkDelete, Truncate, DeleteImmediately, UpdateImmediately } = SchemaOperations
-
-const supportedOperations =  [ List, ListHeaders, Create, Drop, AddColumn, RemoveColumn, Describe, BulkDelete, Truncate, DeleteImmediately, UpdateImmediately ]
+const supportedOperations =  [ List, ListHeaders, Create, Drop, AddColumn, RemoveColumn, Describe, BulkDelete, Truncate, DeleteImmediately, UpdateImmediately, Projection, StartWithCaseSensitive ]
 
 module.exports = { supportedOperations }
