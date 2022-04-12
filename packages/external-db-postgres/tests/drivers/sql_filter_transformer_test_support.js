dddc9e2bd59e7eda33d43b86843ee9d651de9302
Ido <idokah@wix.com>
2022-04-12 12:10:14 +0300

    postgres

--- packages/external-db-postgres/tests/drivers/sql_filter_transformer_test_support.js
+++ packages/external-db-postgres/tests/drivers/sql_filter_transformer_test_support.js
@@ -63,6 +63,11 @@ const givenGreaterThenFilterFor = (filter, column, value) =>
     when(filterParser.transform).calledWith(filter)
                                 .mockReturnValue({ filterExpr: `WHERE ${escapeIdentifier(column)} > $1`, parameters: [value], offset: 2 })
 
+
+const givenNotFilterQueryFor = (filter, column, value) =>
+    when(filterParser.transform).calledWith(filter)
+                                .mockReturnValue({ filterExpr: `WHERE NOT(${escapeIdentifier(column)} = $1)`, parameters: [value], offset: 2 })
+
 const reset = () => {
     filterParser.transform.mockClear()
     filterParser.orderBy.mockClear()
@@ -72,6 +77,7 @@ const reset = () => {
 }
 
 module.exports = { stubEmptyFilterAndSortFor, givenOrderByFor, stubEmptyOrderByFor, stubEmptyFilterFor, 
-                   givenFilterByIdWith, givenAggregateQueryWith, givenAllFieldsProjectionFor, givenProjectionExprFor, givenStartsWithFilterFor, givenGreaterThenFilterFor,
+                   givenFilterByIdWith, givenAggregateQueryWith, givenAllFieldsProjectionFor,
+                   givenProjectionExprFor, givenStartsWithFilterFor, givenGreaterThenFilterFor, givenNotFilterQueryFor,
                    filterParser, reset
 }
--- packages/velo-external-db/test/storage/data_provider.spec.js
+++ packages/velo-external-db/test/storage/data_provider.spec.js
@@ -73,8 +73,8 @@ describe(`Data API: ${currentDbImplementationName()}`, () => {
         await expect( env.dataProvider.find(ctx.collectionName, ctx.filter, ctx.sort, ctx.skip, ctx.limit, ctx.projection) ).resolves.toEqual(expect.arrayContaining([ctx.entity]))
     })
     
-    if (shouldRunOnlyOn(['Mongo', 'DynamoDb', 'MySql'], currentDbImplementationName())) {
-    test('query with not operator filter, will return data', async() => { 
+    if (shouldRunOnlyOn(['Mongo', 'DynamoDb', 'MySql', 'Postgres'], currentDbImplementationName())) {
+    test('query with not operator filter will return data', async() => { 
         await givenCollectionWith([ctx.entity, ctx.anotherEntity], ctx.collectionName, ctx.entityFields)
         env.driver.givenNotFilterQueryFor(ctx.filter, ctx.column.name, ctx.entity[ctx.column.name])
         env.driver.stubEmptyOrderByFor(ctx.sort)
