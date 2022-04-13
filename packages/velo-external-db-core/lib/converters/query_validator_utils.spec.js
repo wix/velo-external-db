18bc2dfaa8cc55aaa5edec6aa41a48f181cd15b6
Idokah <82806105+Idokah@users.noreply.github.com>
2022-04-13 13:51:24 +0300

    split allowedOperators and allowedOperations (#255)
    Co-authored-by: ido kahlon <idokk96@gmail.com>

--- packages/velo-external-db-core/lib/converters/query_validator_utils.spec.js
+++ packages/velo-external-db-core/lib/converters/query_validator_utils.spec.js
@@ -1,7 +1,7 @@
 const { Uninitialized } = require('test-commons')
 const { extractFieldsAndOperators, queryAdapterOperatorsFor } = require ('./query_validator_utils')
 const { AdapterOperators } = require('velo-external-db-commons')
-const { eq, gt, gte, include, lt, lte, ne, string_begins, string_ends, string_contains, urlized, and, matches } = AdapterOperators
+const { eq, gt, gte, include, lt, lte, ne, string_begins, string_ends, string_contains, and, matches } = AdapterOperators
 const { EmptyFilter } = require('./utils')
 const gen = require('../../test/gen')
 const Chance = require('chance')
@@ -42,11 +42,11 @@ describe('Query Validator utils spec', () => {
 
     describe ('queryAdapterOperatorsFor', () => {
         test ('return right operators for type [number]', () => {
-            expect(queryAdapterOperatorsFor('number')).toEqual([eq, ne, gt, gte, lt, lte, include, urlized])
+            expect(queryAdapterOperatorsFor('number')).toEqual([eq, ne, gt, gte, lt, lte, include])
         })
         
         test('return right operators for type [text]', () => {
-            expect(queryAdapterOperatorsFor('text')).toEqual([eq, ne, string_contains, string_begins, string_ends, include, urlized, matches, gt, gte, lt, lte])
+            expect(queryAdapterOperatorsFor('text')).toEqual([eq, ne, string_contains, string_begins, string_ends, include, matches, gt, gte, lt, lte])
         })
 
         test('return right operators for type [boolean]', () => {
@@ -54,7 +54,7 @@ describe('Query Validator utils spec', () => {
         })
 
         test('return right operators for type [url]', () => {
-            expect(queryAdapterOperatorsFor('url')).toEqual([eq, ne, string_contains, include, urlized])
+            expect(queryAdapterOperatorsFor('url')).toEqual([eq, ne, string_contains, include])
         })
 
         test('return right operators for type [datetime]', () => {
