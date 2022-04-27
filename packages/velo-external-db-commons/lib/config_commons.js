5a9269224f598c094118fdb79c85dfa371406445
Ido Kahlon <idokk96@gmail.com>
2022-04-27 19:06:45 +0300

    config_commons to velo-external-db-commons

---
+++ packages/velo-external-db-commons/lib/config_commons.js
@@ -0,0 +1,5 @@
+const objectContainsKey = (obj, key) => typeof obj[key] === 'string' && obj[key].length > 0
+
+const checkRequiredKeys = (obj, requiredKeys) => requiredKeys.filter(key => !objectContainsKey(obj, key))
+
+module.exports = { checkRequiredKeys }
--- packages/velo-external-db-commons/lib/index.js
+++ packages/velo-external-db-commons/lib/index.js
@@ -1,9 +1,11 @@
 const errors = require('./errors')
 const schemaCommons = require('./schema_commons')
 const dataCommons = require('./data_commons')
+const configCommons = require ('./config_commons')
 
 module.exports = {
     errors,
     ...schemaCommons,
     ...dataCommons,
+    ...configCommons
 }
