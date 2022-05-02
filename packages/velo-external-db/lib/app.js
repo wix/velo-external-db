468186f4f1d42d2ce25e537b55b97ec2ef8bb326
Ido Kahlon <idokk96@gmail.com>
2022-05-02 11:17:54 +0300

    remove comments

--- packages/velo-external-db/lib/app.js
+++ packages/velo-external-db/lib/app.js
@@ -48,37 +48,4 @@ initConnector().then(({ externalDbRouter }) => {
 
 const internals = () => ({ server, schemaProvider: _schemaProvider, cleanup: _cleanup, started, reload: initConnector })
 
-// const initMySqlConnector = async () => {
-//     const configReader = create()
-//     const mySqlConfig = await configReader.readConfig()
-//     const mySqlFactory = new MySqlFactory()
-
-//     const { host, user, password, db, authorization } = mySqlConfig
-
-//     const mySqlConnector = await mySqlFactory.create(config)
-//     // const mySqlConnector = new MySqlConnector({ host, user, password, db })
-//     // await mySqlConnector.initProviders()
-//     const externalDbRouter = new ExternalDb({
-//         connector: mySqlConnector,
-//         config: {
-//             authorization: { roleConfig: { collectionLevelConfig: authorization } },
-//             secretKey: process.env.SECRET_KEY
-//         },
-//         hooks: {
-//             // <HookBeforeAction>: async(req, res, opt) => {
-//             // },
-//             // <HookAfterAction>: async(req, res, data, opt) => {
-//             // },
-//         }
-//     })
-
-//     const app = express()
-//     app.set('view engine', 'ejs')
-
-//     app.use(externalDbRouter.router)
-
-//     app.listen(8080, () => console.log('MySql connector listening on port 8080'))
-// }
-
-
 module.exports = { internals }
