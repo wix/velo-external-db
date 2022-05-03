20028b1ac8cffac86287b29eabe2e9f6f2ef6ac3
Max Polsky <maxim.polski@gmail.com>
2022-05-03 14:28:05 +0300

    Firestore small test refactor (#270)

---
+++ packages/external-db-firestore/tests/gen.js
@@ -0,0 +1,20 @@
+const Chance = require('chance')
+const chance = Chance()
+const { AdapterOperators } = require('velo-external-db-commons')
+const { eq, gt, gte, include, lt, lte, ne } = AdapterOperators
+
+
+const randomSupportedFilter = () => {
+    const operator = chance.pickone([ne, lt, lte, gt, gte, include, eq])
+    const fieldName = chance.word()
+    const value = operator === include ? [chance.word(), chance.word(), chance.word(), chance.word(), chance.word()] : chance.word()
+    return {
+        fieldName,
+        operator,
+        value
+    }
+}
+
+
+
+module.exports = { randomSupportedFilter }
