48fba63b762290fc07238e2c69fe4ea1d506d576
Max Polsky <maxim.polski@gmail.com>
2022-05-13 17:57:57 +0300

    refactor: changed the lib folder to src in each package

--- libs/external-db-config/src/service/config_reader.spec.js
+++ libs/external-db-config/src/service/config_reader.spec.js
@@ -1,5 +1,5 @@
 const ConfigReader = require('./config_reader')
-const { Uninitialized } = require('test-commons')
+const { Uninitialized } = require('test-commons/src')
 const driver = require('../../test/drivers/external_db_config_test_support')
 const matchers = require('./config_reader_matchers')
 const gen = require('../../test/gen')
