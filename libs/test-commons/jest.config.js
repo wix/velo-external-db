477bdf20f9fce86de166c4567d0c31ccddb45acb
Max Polsky <maxim.polski@gmail.com>
2022-05-16 13:33:58 +0300

    test: updated jest for test-commons

--- libs/test-commons/jest.config.js
+++ libs/test-commons/jest.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = {
-  displayName: 'velo-external-db-commons',
+  displayName: 'test-commons',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/velo-external-db-commons',
+  coverageDirectory: '../../coverage/libs/test-commons',
 };
