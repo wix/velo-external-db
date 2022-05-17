70bb6addb393955c1f2875c30a40e6f977056c72
Max Polsky <maxim.polski@gmail.com>
2022-05-17 16:45:46 +0300

    NX migration to the mysql package

--- libs/external-db-mysql/jest.config.js
+++ libs/external-db-mysql/jest.config.js
@@ -1,7 +1,18 @@
 module.exports = {
+    displayName: 'external-db-mysql',
     clearMocks: true,
     verbose: true,
-    roots: ['<rootDir>/src'],
-    testRegex: '(.*\\.spec\\.)js$',
+    // roots: ['<rootDir>/src'],
+    preset: '../../jest.preset.js',
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    },
+    transform: {
+      '^.+\\.[tj]s$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/libs/external-db-mysql',
     testEnvironment: 'node'
 }
