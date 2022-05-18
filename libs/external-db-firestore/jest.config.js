82f515d8952b25e01a0881de3031a952166c4558
Max Polsky <maxim.polski@gmail.com>
2022-05-18 16:19:35 +0300

    NX migration to the firestore package

--- libs/external-db-firestore/jest.config.js
+++ libs/external-db-firestore/jest.config.js
@@ -1,8 +1,19 @@
-
 module.exports = {
+    displayName: 'external-db-firestore',
     clearMocks: true,
     verbose: true,
-    roots: ['<rootDir>/src'],
-    testRegex: '(.*\\.spec\\.)js$',
+    // roots: ['<rootDir>/src'],
+    // testRegex: '(.*\\.spec\\.)js$',
+    preset: '../../jest.preset.js',
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    },
+    transform: {
+      '^.+\\.[tj]s$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/libs/external-db-mssql',
     testEnvironment: 'node'
 }
