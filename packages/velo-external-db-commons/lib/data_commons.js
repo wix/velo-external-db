3a73e0961454856346842f5ea41d1d499d32188f
Ido Kahlon <82806105+Idokah@users.noreply.github.com>
2022-05-29 10:44:32 +0300

    replace for loop with promise.reduce (#279)
    * replace for loop with promise.reduce
    
    * rename schemaAware to dataService in serviceContext

--- packages/velo-external-db-commons/lib/data_commons.js
+++ packages/velo-external-db-commons/lib/data_commons.js
@@ -95,14 +95,7 @@ const specItemToRegex = (spec) => {
     }
 }
 
-const clone = (data) => {
-    if (Array.isArray(data))
-        return [...data]
-    if (isObject(data))
-        return { ...data }
-    return data
-}
 
 module.exports = { EmptyFilter, EmptySort, patchDateTime, asParamArrays, isObject, isDate,
                      updateFieldsFor, isEmptyFilter, AdapterOperators, AdapterFunctions,
-                     extractGroupByNames, extractProjectionFunctionsObjects, isNull, specArrayToRegex, clone }
+                     extractGroupByNames, extractProjectionFunctionsObjects, isNull, specArrayToRegex }
