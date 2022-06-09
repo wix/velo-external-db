36c2ddcc3ba7f2875ca8ff2a1d5ba2112fa20974
Ido Kahlon <idokk96@gmail.com>
2022-06-09 14:14:57 +0300

    export DbConnector properly

--- libs/velo-external-db-commons/src/index.ts
+++ libs/velo-external-db-commons/src/index.ts
@@ -2,7 +2,7 @@ import * as errors from './libs/errors'
 import * as schemaCommons from './libs/schema_commons'
 import * as dataCommons from './libs/data_commons'
 import * as configCommons from './libs/config_commons'
-import * as DbConnector from './libs/db_connector'
+import DbConnector from './libs/db_connector'
 
 export = {
     errors,
