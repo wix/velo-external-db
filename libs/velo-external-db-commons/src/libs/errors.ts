4969b0ecc6de9d9a67f7ea776a9f7654dd843373
Ido Kahlon <idokk96@gmail.com>
2022-06-13 09:54:09 +0300

    errors.ts

--- libs/velo-external-db-commons/src/libs/errors.ts
+++ libs/velo-external-db-commons/src/libs/errors.ts
@@ -1,84 +1,84 @@
 class BaseHttpError extends Error {
-    status: any
-    constructor(message: string | undefined, status: any) {
+    status: number
+    constructor(message: string, status: number) {
         super(message)
         this.status = status
     }
 }
 
 class UnauthorizedError extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 401)
     }
 }
 
 class CollectionDoesNotExists extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 404)
     }
 }
 
 class CollectionAlreadyExists extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class FieldAlreadyExists extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class ItemAlreadyExists extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class FieldDoesNotExist extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 404)
     }
 }
 
 class CannotModifySystemField extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class InvalidQuery extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class InvalidRequest extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 400)
     }
 }
 
 class DbConnectionError extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 500)
     }
 }
 class ItemNotFound extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 404)
     }
 }
 
 class UnsupportedOperation extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 405)
     }
 }
 
 class UnsupportedDatabase extends BaseHttpError {
-    constructor(message: any) {
+    constructor(message: string) {
         super(message, 405)
     }
 }
