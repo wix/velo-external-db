302a11502a06d8b3704f6eae609dd55bc75390b0
Ido Kahlon <idokk96@gmail.com>
2022-06-19 17:44:36 +0300

    project.json, jest config, typo

--- libs/external-db-dynamodb/src/connection_provider.ts
+++ libs/external-db-dynamodb/src/connection_provider.ts
@@ -18,7 +18,7 @@ const extraOptions  = (cfg: DynamoConfig) => {
 }
 
 
-export default const init = (cfg: DynamoConfig, _cfgOptions: {[x: string]: any}) : DbProviders<DynamoDB> => { 
+export default (cfg: DynamoConfig, _cfgOptions?: {[x: string]: any}) : DbProviders<DynamoDB> => { 
     const options = _cfgOptions || {}
     const client = new DynamoDB({ region: cfg.region, ...extraOptions(cfg), ...options })
     const databaseOperations = new DatabaseOperations(client)
