811e61311ce919008c6e7cdf7513e3165594a238
Ido Kahlon <idokk96@gmail.com>
2022-06-19 17:37:02 +0300

    connection provider + index.ts

---
+++ libs/external-db-dynamodb/src/connection_provider.ts
@@ -0,0 +1,31 @@
+import SchemaProvider from'./dynamo_schema_provider'
+import DataProvider from './dynamo_data_provider'
+import FilterParser from './sql_filter_transformer'
+import DatabaseOperations from './dynamo_operations'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DbProviders } from '@wix-velo/velo-external-db-types'
+
+export interface DynamoConfig {
+    region: string
+    endpoint?: string
+    [x: string]: any
+}
+
+const extraOptions  = (cfg: DynamoConfig) => { 
+    if (cfg.endpoint)
+        return { endpoint: cfg.endpoint }
+    return {}
+}
+
+
+export default const init = (cfg: DynamoConfig, _cfgOptions: {[x: string]: any}) : DbProviders<DynamoDB> => { 
+    const options = _cfgOptions || {}
+    const client = new DynamoDB({ region: cfg.region, ...extraOptions(cfg), ...options })
+    const databaseOperations = new DatabaseOperations(client)
+
+    const filterParser = new FilterParser()
+    const dataProvider = new DataProvider(client, filterParser)
+    const schemaProvider = new SchemaProvider(client)
+
+    return { dataProvider, schemaProvider, databaseOperations, connection: client, cleanup: () => {} }
+}
