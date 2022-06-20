0da544159b2bbacd19b03fe6020685def92dbeeb
Ido Kahlon <idokk96@gmail.com>
2022-06-20 13:42:50 +0300

    connection provider, index

---
+++ libs/external-db-airtable/src/connection_provider.ts
@@ -0,0 +1,30 @@
+import DataProvider from './airtable_data_provider'
+import SchemaProvider from './airtable_schema_provider'
+import FilterParser from './sql_filter_transformer'
+import DatabaseOperations from './airtable_operations'
+import * as Airtable from 'airtable'
+import { DbProviders } from '@wix-velo/velo-external-db-types'
+
+
+interface AirtableConfig {
+    apiPrivateKey: string
+    baseId: string
+    metaApiKey: string
+    baseUrl?: string
+}
+
+const extraOptions = (cfg: AirtableConfig) => {
+    if (cfg.baseUrl)
+        return { endpointUrl: cfg.baseUrl }
+    return {}
+}
+
+export default async (cfg: AirtableConfig, _cfgOptions: { [x: string]: any }): Promise<DbProviders<Airtable.Base>> => {
+    const options = _cfgOptions || {}
+    const airtableBase = new Airtable({ apiKey: cfg.apiPrivateKey, ...extraOptions(cfg), ...options }).base(cfg.baseId)
+    const databaseOperations = new DatabaseOperations(airtableBase)
+    const filterParser = new FilterParser()
+    const dataProvider = new DataProvider(airtableBase, filterParser)
+    const schemaProvider = new SchemaProvider(airtableBase, { apiKey: cfg.apiPrivateKey, metaApiKey: cfg.metaApiKey, baseUrl: cfg.baseUrl })
+    return { dataProvider, schemaProvider, connection: airtableBase, databaseOperations, cleanup: async () => { } }
+}
---
+++ libs/external-db-airtable/src/index.ts
@@ -0,0 +1,29 @@
+export { default as SchemaProvider } from './airtable_schema_provider'
+export { default as DataProvider } from './airtable_data_provider'
+export { default as FilterParser } from './sql_filter_transformer'
+export { default as SchemaColumnTranslator } from './sql_schema_translator'
+export { default as init } from './connection_provider'
+export { default as DatabaseOperations } from './airtable_operations'
+export { supportedOperations } from './supported_operations'
+export { app as mockServer } from '../tests/drivers/mock_air_table'
+
+import init from './connection_provider'
+import { AirtableConfigValidator } from './airtable_config_validator'
+import { DbConnector } from '@wix-velo/velo-external-db-commons'
+
+export const driver = () => require('../tests/drivers/sql_filter_transformer_test_support')
+export const opsDriver = () => require('../tests/drivers/db_operations_test_support')
+
+export class AirtableConnector extends DbConnector {
+    type: string
+    constructor() {
+        super(AirtableConfigValidator, init)
+        this.type = 'airtable'
+    }
+}
+
+export const airtableFactory = async (config: any, options: any) => {
+    const connector = new AirtableConnector()
+    const { connection, cleanup, ...providers } = await connector.initialize(config, options)
+    return { connector, connection, providers, cleanup }
+}
