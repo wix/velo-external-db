7ad2917cec3e7c2efee11fe3d7d7f97106e862c7
Ido Kahlon <idokk96@gmail.com>
2022-06-21 18:15:51 +0300

    utils

--- libs/velo-external-db-core/src/utils/router_utils.build.ts
+++ libs/velo-external-db-core/src/utils/router_utils.build.ts
@@ -1,18 +1,17 @@
-const path = require('path')
-const ejs = require('ejs')
+import * as path from 'path'
+import * as ejs from 'ejs'
+import { AnyFixMe } from '@wix-velo/velo-external-db-types'
 const fs = require('fs').promises
 
 const getAppInfoTemplate = async() => {
     return await fs.readFile(path.join( __dirname, 'views', 'index.ejs'), 'utf8')
 }
 
-const getAppInfoPage = async(appInfo) => {
+export const getAppInfoPage = async(appInfo: AnyFixMe) => {
     const appInfoTemplate = await getAppInfoTemplate()
     const appInfoPage = ejs.render(appInfoTemplate, appInfo)
     return appInfoPage
 }
-
-module.exports = { getAppInfoPage }
 
 
 
