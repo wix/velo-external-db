a6406f02e171cbd4001c51661ab8bf176d4dbc72
Ido Kahlon <idokk96@gmail.com>
2022-06-22 17:56:53 +0300

    schema service

--- libs/velo-external-db-core/src/service/schema.ts
+++ libs/velo-external-db-core/src/service/schema.ts
@@ -1,8 +1,12 @@
-const { asWixSchema, asWixSchemaHeaders, allowedOperationsFor, appendQueryOperatorsTo, SchemaOperations, errors } = require('@wix-velo/velo-external-db-commons')
+import { asWixSchema, asWixSchemaHeaders, allowedOperationsFor, appendQueryOperatorsTo, SchemaOperations, errors } from '@wix-velo/velo-external-db-commons'
+import { InputField, ISchemaProvider, Table } from '@wix-velo/velo-external-db-types'
+import CacheableSchemaInformation from './schema_information'
 const { Create, AddColumn, RemoveColumn } = SchemaOperations
 
-class SchemaService {
-    constructor(storage, schemaInformation) {
+export default class SchemaService {
+    storage: ISchemaProvider
+    schemaInformation: CacheableSchemaInformation
+    constructor(storage: any, schemaInformation: any) {
         this.storage = storage
         this.schemaInformation = schemaInformation
     }
@@ -16,48 +20,49 @@ class SchemaService {
 
     async listHeaders() {
         const collections = await this.storage.listHeaders()
-        return { schemas: collections.map( asWixSchemaHeaders ) }
+        return { schemas: collections.map((collection)=> asWixSchemaHeaders(collection.id)) }
     }
 
-    async find(collectionNames) {
-        const dbs = await Promise.all(collectionNames.map(async collectionName => ({ id: collectionName, fields: await this.schemaInformation.schemaFieldsFor(collectionName) })))
+    async find(collectionNames: string[]) {
+        const dbs: Table[] = await Promise.all(collectionNames.map(async (collectionName: string) => ({ id: collectionName, fields: await this.schemaInformation.schemaFieldsFor(collectionName) })))
         const dbsWithAllowedOperations = this.appendAllowedOperationsTo(dbs)
 
         return { schemas: dbsWithAllowedOperations.map( asWixSchema ) }
     }
 
-    async create(collectionName) {
+    async create(collectionName: string) {
         await this.validateOperation(Create)
         await this.storage.create(collectionName)
         await this.schemaInformation.refresh()
         return {}
     }
 
-    async addColumn(collectionName, column) {
+    async addColumn(collectionName: string, column: InputField) {
         await this.validateOperation(AddColumn)
         await this.storage.addColumn(collectionName, column)
         await this.schemaInformation.refresh()
         return {}
     }
 
-    async removeColumn(collectionName, columnName) {
+    async removeColumn(collectionName: string, columnName: string) {
         await this.validateOperation(RemoveColumn)
         await this.storage.removeColumn(collectionName, columnName)
         await this.schemaInformation.refresh()
         return {}
     }
 
-    appendAllowedOperationsTo(dbs) {
+    appendAllowedOperationsTo(dbs: Table[]) {
         const allowedSchemaOperations = this.storage.supportedOperations()
-        return dbs.map(db => ({
-            ...db, 
+        return dbs.map((db: Table) => ({
+            ...db,
             allowedSchemaOperations,
             allowedOperations: allowedOperationsFor(db),
             fields: appendQueryOperatorsTo(db.fields)
         }))
     }
+
     
-    async validateOperation(operationName) {
+    async validateOperation(operationName: SchemaOperations) {
         const allowedSchemaOperations = this.storage.supportedOperations()
 
         if (!allowedSchemaOperations.includes(operationName)) 
@@ -65,5 +70,3 @@ class SchemaService {
     }
 
 }
-
-module.exports = SchemaService
