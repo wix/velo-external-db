fef3bc8fb518dddb33872d80ad0b805a05ba1d0a
Ido Kahlon <idokk96@gmail.com>
2022-06-22 11:14:32 +0300

    data service

--- libs/velo-external-db-core/src/converters/data_utils.ts
+++ libs/velo-external-db-core/src/converters/data_utils.ts
@@ -1,12 +1,12 @@
 import { isDate } from '@wix-velo/velo-external-db-commons'
-import { Item } from '@wix-velo/velo-external-db-types'
+import { Item, ItemWithId } from '@wix-velo/velo-external-db-types'
 import * as crypto from 'crypto'
 
 export const asWixData = (e: any) => generateIdsIfNeeded(packDates(e))
 
-export const generateIdsIfNeeded = (item: Item) => {
+export const generateIdsIfNeeded = (item: Item): ItemWithId => {
     if ('_id' in item)
-        return item
+        return item as ItemWithId
     const sha = crypto.createHash('sha1')
     const fieldsConcat = Object.values(item).join('')
     return { ...item, _id: sha.update(fieldsConcat).digest('base64') }
--- libs/velo-external-db-core/src/utils/data_utils.ts
+++ libs/velo-external-db-core/src/utils/data_utils.ts
@@ -1,7 +1,8 @@
 import { AdapterOperators } from '@wix-velo/velo-external-db-commons'
+import { AdapterFilter, AdapterOperator } from '@wix-velo/velo-external-db-types'
 
-export const getByIdFilterFor = (itemId: string) => ({
+export const getByIdFilterFor = (itemId: string): AdapterFilter => ({
     fieldName: '_id',
-    operator: AdapterOperators.eq,
+    operator: AdapterOperators.eq as AdapterOperator,
     value: itemId    
 })
