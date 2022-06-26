639e5241f6d9638552a820e5e6699e8bd31a7c0d
Ido Kahlon <idokk96@gmail.com>
2022-06-26 11:38:41 +0300

    nop

--- libs/velo-external-db-core/src/health/app_info.ts
+++ libs/velo-external-db-core/src/health/app_info.ts
@@ -10,7 +10,7 @@ export const maskSensitiveData = (cfg: {[key: string]: any}) => {
 
 // 1. fix config reader config client when moving configReader to ts
 // 2. operation service - when declaring it as OperationService, there's a problem with mock and tests, create interface?
-export const appInfoFor = async(operationService: any, configReaderClient: AnyFixMe) => {
+export const appInfoFor = async(operationService: AnyFixMe, configReaderClient: AnyFixMe) => {
     const connectionStatus = await operationService.connectionStatus()
     const { message: configReaderStatus, authorizationMessage: authorizationConfigStatus } = await configReaderClient.configStatus()
     
