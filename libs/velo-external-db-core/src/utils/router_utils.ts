69b4813487971096a86b183ed78d993fb36f4fbe
Ido Kahlon <idokk96@gmail.com>
2022-07-04 12:37:29 +0300

    patch ejs file path

--- libs/velo-external-db-core/src/utils/router_utils.ts
+++ libs/velo-external-db-core/src/utils/router_utils.ts
@@ -2,8 +2,14 @@ import path = require('path')
 import ejs = require('ejs')
 import {promises as fs} from 'fs'
 
-const getAppInfoTemplate = async() => {
-    return await fs.readFile(path.join( __dirname, '..', 'views', 'index.ejs'), 'utf8')
+const getAppInfoTemplate = async() => { // TODO: fix this hack!
+    try {
+        return await fs.readFile(path.join( __dirname, '..', 'views', 'index.ejs'), 'utf8')
+    } catch (err) {
+        return await fs.readFile(path.join( __dirname, 'views', 'index.ejs'), 'utf8')
+    }
+
+    
 }
 
 export const getAppInfoPage = async(appInfo: any) => {
