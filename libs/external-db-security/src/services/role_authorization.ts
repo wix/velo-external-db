93552571525a29abcb26c415ea1bb87a12c35f8d
Ido Kahlon <82806105+Idokah@users.noreply.github.com>
2022-07-12 14:03:01 +0300

    Security to ts (#318)
    * role authorization service
    
    * role config types
    
    * gen
    
    * index, project.json and tsconfig
    
    * change imports

--- libs/external-db-security/src/services/role_authorization.ts
+++ libs/external-db-security/src/services/role_authorization.ts
@@ -1,41 +1,44 @@
-const { errors } = require('@wix-velo/velo-external-db-commons')
+import { CollectionPermissions, VeloRole, WixDataRole } from '@wix-velo/velo-external-db-types'
+
+import { errors } from '@wix-velo/velo-external-db-commons'
 const { UnauthorizedError } = errors
 const DefaultPolicies = ['Admin']
 
-class RoleAuthorizationService {
-    constructor(config) {
-        this.config = config || []
+export default class RoleAuthorizationService {
+    config: CollectionPermissions[]
+    constructor(config: CollectionPermissions[] = []) {
+        this.config = config
     }
 
-    authorizeRead(collectionName, role) {
+    authorizeRead(collectionName: string, role: WixDataRole) {
         const readPolicies = this.readPoliciesFor(collectionName)
 
         if (!readPolicies.includes(this.dataToVeloRole(role))) throw new UnauthorizedError('You are not authorized')
     }
     
-    authorizeWrite(collectionName, role) {
+    authorizeWrite(collectionName: string, role: WixDataRole) {
         const writePolicies = this.writePoliciesFor(collectionName)
         
         if (!writePolicies.includes(this.dataToVeloRole(role))) throw new UnauthorizedError('You are not authorized')
     }
 
-    setConfig(config) {
+    setConfig(config: CollectionPermissions[]) {
         this.config = config
     }
 
-    policiesFor(collectionName) {
+    policiesFor(collectionName: string) {
         return this.config.find(p => p.id === collectionName)
     }
 
-    readPoliciesFor(collectionName) {
+    readPoliciesFor(collectionName: string) {
         return this.policiesFor(collectionName)?.read || DefaultPolicies
     }
 
-    writePoliciesFor(collectionName) {
+    writePoliciesFor(collectionName: string) {
         return this.policiesFor(collectionName)?.write || DefaultPolicies
     }
 
-    dataToVeloRole(role) { 
+    dataToVeloRole(role: WixDataRole): VeloRole { 
         switch (role) {
             case 'OWNER':
             case 'BACKEND_CODE':
@@ -49,5 +52,3 @@ class RoleAuthorizationService {
         }
     }     
 }
-
-module.exports = RoleAuthorizationService
