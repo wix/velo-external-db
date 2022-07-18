0c75802416e6518d72799f8d1fb73e5096bd61ad
Ido Kahlon <idokk96@gmail.com>
2022-07-18 15:20:27 +0300

    app.ts, jest, project.json, tsconfig

--- apps/velo-external-db/jest.config.ts
+++ apps/velo-external-db/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     displayName: 'velo-external-db',
     preset: '../../jest.preset.js',
     clearMocks: true,
--- apps/velo-external-db/src/app.ts
+++ apps/velo-external-db/src/app.ts
@@ -1,16 +1,18 @@
-const express = require('express')
-const { create, readCommonConfig } = require('@wix-velo/external-db-config')
-const { ExternalDbRouter } = require('@wix-velo/velo-external-db-core')
-const { engineConnectorFor } = require('./storage/factory')
+import express = require('express')
+import { create, readCommonConfig } from '@wix-velo/external-db-config'
+import { ExternalDbRouter } from '@wix-velo/velo-external-db-core'
+import { engineConnectorFor } from './storage/factory'
+import { Server } from 'http'
+import { ConnectionCleanUp, ISchemaProvider } from '@wix-velo/velo-external-db-types'
+import { DataHooks, SchemaHooks } from 'libs/velo-external-db-core/src/types'
 
 let started = false
-let server, _schemaProvider, _cleanup
-let externalDbRouter
+let server: Server, _schemaProvider: ISchemaProvider, _cleanup: ConnectionCleanUp
+let externalDbRouter: ExternalDbRouter
 
 
 
-
-const initConnector = async(hooks) => { 
+const initConnector = async (hooks?: { dataHooks: DataHooks, schemaHooks: SchemaHooks }) => {
     const { vendor, type: adapterType } = readCommonConfig()
     const configReader = create()
     const { authorization, secretKey, ...dbConfig } = await configReader.readConfig()
@@ -31,11 +33,11 @@ const initConnector = async(hooks) => {
         hooks
     })
 
-    _cleanup = async() => {
+    _cleanup = async () => {
         await cleanup()
     }
     _schemaProvider = providers.schemaProvider
-    
+
     return { externalDbRouter }
 }
 
@@ -48,6 +50,4 @@ initConnector().then(({ externalDbRouter }) => {
     started = true
 })
 
-const internals = () => ({ server, schemaProvider: _schemaProvider, cleanup: _cleanup, started, reload: initConnector, externalDbRouter })
-
-module.exports = { internals }
+export const internals = () => ({ server, schemaProvider: _schemaProvider, cleanup: _cleanup, started, reload: initConnector, externalDbRouter })
