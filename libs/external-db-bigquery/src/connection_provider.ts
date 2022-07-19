6f117289ed7fc2f5bcabdc83bf7ed8f740357327
Max Polsky <maxim.polski@gmail.com>
2022-07-19 17:03:50 +0300

    added DbProviders type

--- libs/external-db-bigquery/src/connection_provider.ts
+++ libs/external-db-bigquery/src/connection_provider.ts
@@ -1,11 +1,12 @@
-import { BigQuery } from '@google-cloud/bigquery'
+import { BigQuery, Dataset } from '@google-cloud/bigquery'
 import FilterParser from './sql_filter_transformer'
 import DatabaseOperations from './bigquery_operations'
 import SchemaProvider from './bigquery_schema_provider'
 import DataProvider from './bigquery_data_provider'
 import { BigQueryConfig } from './types'
+import { DbProviders } from '@wix-velo/velo-external-db-types'
 
-export default ({ projectId, databaseId }: BigQueryConfig) => {
+export default ({ projectId, databaseId }: BigQueryConfig): DbProviders<Dataset> => {
     const bigquery = new BigQuery()
     const pool = bigquery.dataset(databaseId)
 
