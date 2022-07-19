1caf60a47661cdacb23bbb4357ff3b9c28b12449
Max Polsky <maxim.polski@gmail.com>
2022-07-19 17:31:04 +0300

    fixed the DatabaseOperations class

--- libs/external-db-bigquery/src/bigquery_operations.ts
+++ libs/external-db-bigquery/src/bigquery_operations.ts
@@ -2,26 +2,32 @@
 
 import { Dataset } from '@google-cloud/bigquery'
 import { errors } from '@wix-velo/velo-external-db-commons'
-const { DbConnectionError } = errors
+import { IDatabaseOperations, ValidateConnectionResult } from '@wix-velo/velo-external-db-types'
+const { DbConnectionError, UnauthorizedError } = errors
 
 const notThrowingTranslateErrorCodes = (err: any) => {
     switch (err.code) {
         case 404:
             return new DbConnectionError(err.message)
         default :
-            return new Error(`Default ${err.code} ${err.message}`)
+            return new UnauthorizedError(`${err.code} ${err.message}`)
     }
 }
 
-export default class DatabaseOperations {
+export default class DatabaseOperations implements IDatabaseOperations {
     pool: Dataset
     constructor(pool: Dataset) {
         this.pool = pool
     }
 
-    async validateConnection() {
-        return await this.pool.getMetadata().then(() => ({ valid: true }))
-                              .catch((e) => ({ valid: false, error: notThrowingTranslateErrorCodes(e) }))
+    async validateConnection(): Promise<ValidateConnectionResult>  {
+        try {
+            await this.pool.getMetadata()
+            return { valid: true }
+            
+        } catch (error) {
+            return { valid: false, error: notThrowingTranslateErrorCodes(error) }
+        }
     }
 }
 
