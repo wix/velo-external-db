f4d023dc00dd87bcef2a0ae2581ff53e54c3bfb7
Ido Kahlon <idokk96@gmail.com>
2022-07-26 17:31:44 +0300

    provider resources, delete engines

--- apps/velo-external-db/test/resources/engines/docker_support.ts
+++
@@ -1,9 +0,0 @@
-import * as compose from 'docker-compose'
-
-export const runImage = async(image: string, showLogs?: boolean) => {
-    await compose.upOne(image, { cwd: __dirname, log: true, commandOptions: [['--force-recreate', '--remove-orphans']] })
-    if (showLogs) {
-        await compose.logs(image, { cwd: __dirname, log: true })
-    }
-}
-export const stopImage = async(image: string) => await compose.stopOne(image, { cwd: __dirname, log: true })
--- apps/velo-external-db/test/resources/provider_resources.ts
+++ apps/velo-external-db/test/resources/provider_resources.ts
@@ -2,31 +2,22 @@ import { Uninitialized } from '@wix-velo/test-commons'
 import { suiteDef } from './test_suite_definition'
 
 import * as mysql from '@wix-velo/external-db-mysql'
-import * as mysqlTestEnv from './engines/mysql_resources'
 
 import * as spanner from '@wix-velo/external-db-spanner'
-import * as spannerTestEnv from './engines/spanner_resources'
 
 import * as postgres from '@wix-velo/external-db-postgres'
-import * as postgresTestEnv from './engines/postgres_resources'
 
 import * as firestore from '@wix-velo/external-db-firestore'
-import * as firestoreTestEnv from './engines/firestore_resources'
 
 import * as mssql from '@wix-velo/external-db-mssql'
-import * as mssqlTestEnv from './engines/mssql_resources'
 
 import * as mongo from '@wix-velo/external-db-mongo'
-import * as mongoTestEnv from './engines/mongo_resources'
 
 import * as airtable from '@wix-velo/external-db-airtable'
-import * as airtableEnv from './engines/airtable_resources'
 
 import * as dynamo from '@wix-velo/external-db-dynamodb'
-import * as dynamoTestEnv from './engines/dynamodb_resources'
 
 import * as bigquery from '@wix-velo/external-db-bigquery'
-import * as bigqueryTestEnv from './engines/bigquery_resources'
 import { AnyFixMe, ConnectionCleanUp, IDataProvider, ISchemaProvider } from '@wix-velo/velo-external-db-types'
 
 // const googleSheet = require('@wix-velo/external-db-google-sheets')
@@ -44,14 +35,15 @@ export const env: {
     driver: Uninitialized,
 }
 
-const dbInit = async(testEnv: any, impl: any) => {
-    await testEnv.cleanup()
+const dbInit = async(impl: any) => {
+    const testResources = impl.testResources
+    await testResources.cleanup()
 
-    const { pool, cleanup } = await testEnv.connection()
+    const { pool, cleanup } = await testResources.connection()
     const driver = impl.driver()
         
     env.dataProvider = new impl.DataProvider(pool, driver.filterParser)
-    env.schemaProvider = new impl.SchemaProvider(pool, testEnv.schemaProviderTestVariables?.() )
+    env.schemaProvider = new impl.SchemaProvider(pool, testResources.schemaProviderTestVariables?.() )
     env.driver = driver
     env.cleanup = cleanup
 }
@@ -63,15 +55,15 @@ export const dbTeardown = async() => {
     env.driver = Uninitialized
 }
 
-const postgresTestEnvInit = async() => await dbInit(postgresTestEnv, postgres)
-const mysqlTestEnvInit = async() => await dbInit(mysqlTestEnv, mysql)
-const spannerTestEnvInit = async() => await dbInit(spannerTestEnv, spanner)
-const firestoreTestEnvInit = async() => await dbInit(firestoreTestEnv, firestore)
-const mssqlTestEnvInit = async() => await dbInit(mssqlTestEnv, mssql)
-const mongoTestEnvInit = async() => await dbInit(mongoTestEnv, mongo)
-const airTableTestEnvInit = async() => await dbInit(airtableEnv, airtable)
-const dynamoTestEnvInit = async() => await dbInit(dynamoTestEnv, dynamo)
-const bigqueryTestEnvInit = async() => await dbInit(bigqueryTestEnv, bigquery)
+const postgresTestEnvInit = async() => await dbInit(postgres)
+const mysqlTestEnvInit = async() => await dbInit(mysql)
+const spannerTestEnvInit = async() => await dbInit(spanner)
+const firestoreTestEnvInit = async() => await dbInit(firestore)
+const mssqlTestEnvInit = async() => await dbInit(mssql)
+const mongoTestEnvInit = async() => await dbInit(mongo)
+const airTableTestEnvInit = async() => await dbInit(airtable)
+const dynamoTestEnvInit = async() => await dbInit(dynamo)
+const bigqueryTestEnvInit = async() => await dbInit(bigquery)
 // const googleSheetTestEnvInit = async() => await dbInit(googleSheetTestEnv, googleSheet)
 
 const testSuits = {
