fd1fbca380374cc6afd540637a02b5d8157a22da
Ido Kahlon <idokk96@gmail.com>
2022-07-26 15:05:06 +0300

    google-sheets

---
+++ libs/external-db-google-sheets/tests/e2e-testkit/google_sheets_resources.ts
@@ -0,0 +1,32 @@
+const init = require('../../src/connection_provider')
+const { app: mockServer} = require ('../mock_google_sheets_api')
+import { Server } from 'http'
+export { supportedOperations } from '@wix-velo/external-db-google-sheets'
+
+let _server: Server, port = 1502
+
+export const connection = async() => {
+    const { connection, schemaProvider, cleanup } = await init({ sheetId: 'test-sheet-id' })
+    return { pool: connection, schemaProvider, cleanup: cleanup }
+}   
+
+export const cleanup = async() => {
+    // todo: add cleanup logic
+}
+
+export const initEnv = async() => {    
+    _server = mockServer.listen(port)
+}
+
+export const shutdownEnv = async() => {
+    _server.close()
+}
+
+export const setActive = () => {
+    process.env['TYPE'] = 'google-sheet'
+    process.env['CLIENT_EMAIL'] = 'client-email'
+    process.env['SHEET_ID'] = 'test-sheet-id'
+    process.env['API_PRIVATE_KEY'] = 'test-api-key'
+}
+
+export const name = 'google-sheets'
