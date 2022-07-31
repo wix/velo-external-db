269973167a954651a3b676361d7c2028d7cfd7ac
Max Polsky <maxim.polski@gmail.com>
2022-07-31 10:34:41 +0300

    refactror: index.ts file

--- libs/external-db-google-sheets/src/index.ts
+++ libs/external-db-google-sheets/src/index.ts
@@ -1,26 +1,25 @@
-import { DbConnector } from '@wix-velo/velo-external-db-commons'
-import SchemaProvider from './google_sheet_schema_provider'
-import DataProvider from './google_sheet_data_provider'
-import ConfigValidator from './google_sheet_config_validator'
+export { default as SchemaProvider } from './google_sheet_schema_provider'
+export { default as DataProvider } from './google_sheet_data_provider'
+export { supportedOperations } from './supported_operations'
+export { default as init } from './connection_provider'
+export { app as mockServer, cleanupSheets } from '../tests/mock_google_sheets_api'
+
 import init from './connection_provider'
-import { app as mockServer, cleanupSheets } from '../tests/mock_google_sheets_api'
-import { supportedOperations } from './supported_operations'
+import { DbConnector } from '@wix-velo/velo-external-db-commons'
+import ConfigValidator from './google_sheet_config_validator'
 
-const driver = () => require('../tests/sql_filter_transformer_test_support.js')
+// export const driver = () => require('../tests/sql_filter_transformer_test_support.js')
+// export const opsDriver = () => require('../tests/db_operations_test_support')
 
-class GoogleSheetConnector extends DbConnector {
+export class GoogleSheetConnector extends DbConnector {
     constructor() {
         super(ConfigValidator, init)
         this.type = 'google-sheet'
     }
 }
 
-const googleSheetFactory = async(config: any, options: any) => {
+export const googleSheetFactory = async(config: any, options: any) => {
     const connector = new GoogleSheetConnector()
     const { connection, cleanup, ...providers } = await connector.initialize(config, options)
     return { connector, connection, providers, cleanup }
 }
-
-
-
-export { SchemaProvider, DataProvider, init, mockServer, driver, cleanupSheets, supportedOperations, GoogleSheetConnector, googleSheetFactory }
