84e368cbbfe3fe271c28dee854ebbe88544e6005
Max Polsky <maxim.polski@gmail.com>
2022-08-04 17:33:30 +0300

    feat: new errors

--- libs/external-db-google-sheets/src/google_sheet_exception_translator.ts
+++ libs/external-db-google-sheets/src/google_sheet_exception_translator.ts
@@ -1,12 +1,25 @@
 import { errors } from '@wix-velo/velo-external-db-commons'
-const { FieldAlreadyExists, UnrecognizedError } = errors
+const { FieldAlreadyExists, UnrecognizedError, DbConnectionError } = errors
 
 export const notThrowingTranslateErrorCodes = (err: any) => {
+    
+    if (!err.code) {
+        if (err.message.includes('entity was not found'))
+            return new DbConnectionError('Sheet ID is invalid')
+    }
+
     switch (err.code) {
-        default :
-            return new UnrecognizedError(`${err}`)
+        case 'ERR_OSSL_PEM_BAD_BASE64_DECODE':
+        case 'ERR_OSSL_PEM_NO_START_LINE':
+        case 'ERR_OSSL_PEM_BAD_END_LINE':
+            return new DbConnectionError(`API key is invalid: ${err.message}`)
+        case '400':
+            return new DbConnectionError('Client email is invalid')
+        default : 
+            return new UnrecognizedError(`${err.message}`)
     }
 }
+
 export const translateErrorCodes = (err: any) => {
 
     if (err.message.includes('Duplicate header detected')) {
