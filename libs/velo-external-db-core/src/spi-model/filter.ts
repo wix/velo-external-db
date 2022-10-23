adcba3f44243ac6486258023345f62c3fde316ef
michaelir <michaelir@wix.com>
2022-10-23 12:17:57 +0300

    wip

---
+++ libs/velo-external-db-core/src/spi-model/filter.ts
@@ -0,0 +1,42 @@
+
+// type PrimitveType = number | string | boolean
+// type PrimitveTypeArray = PrimitveType[]
+
+// interface Filter {
+//    root: And
+// }
+
+
+// interface ToAdapterType {
+//     toAdapter(): void
+// }
+
+// type Comperator = Eq | Ne | Lt
+
+// interface FieldComperator {
+//     [fieldName: string]: Comperator | PrimitveType | PrimitveTypeArray
+// }
+
+// interface Eq {
+//     $eq: PrimitveType
+// }
+
+// interface Ne {
+//     $ne: PrimitveType
+// }
+
+// interface Lt {
+//     $lt: number
+// }
+
+// interface And {
+//     $and: Array<FieldComperator | And | Or | Not>
+// }
+
+// interface Or {
+//     $or: Array<FieldComperator | And | Or | Not>
+// }
+
+// interface Not {
+//     $not: FieldComperator | And | Or | Not
+// }
