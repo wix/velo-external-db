227d424ea4f20d19e648c6b5363ba6c11ef658fe
Ido Kahlon <idokk96@gmail.com>
2022-11-27 12:15:57 +0200

    create index_provider - separate from schema_provider

---
+++ libs/external-db-mysql/src/mysql_index_provider.ts
@@ -0,0 +1,48 @@
+import { promisify } from 'util'
+import { Pool as MySqlPool } from 'mysql'
+import { MySqlQuery } from './types'
+import { DomainIndex } from '@wix-velo/velo-external-db-types'
+import { escapeId, escapeTable } from './mysql_utils'
+import { translateErrorCodes } from './sql_exception_translator'
+
+export default class IndexProvider {
+    pool: MySqlPool
+    query: MySqlQuery
+    constructor(pool: any) {
+        this.pool = pool
+        this.query = promisify(this.pool.query).bind(this.pool)
+    }
+
+    async listIndexes(collectionName: string): Promise<DomainIndex[]> {
+        const res = await this.query(`SHOW INDEXES FROM ${escapeTable(collectionName)}`)
+                              .catch( translateErrorCodes )
+        const indexes: {[x:string]: DomainIndex} = {}
+        res.forEach((r: { Key_name: string; Column_name: string; Non_unique: number }) => {
+            if (!indexes[r.Key_name]) {
+                indexes[r.Key_name] = {
+                    collectionName,
+                    name: r.Key_name,
+                    columns: [],
+                    isUnique: r.Non_unique === 0,
+                    caseInsensitive: true, // by default true but can be changed by the user - need to check.
+                    order: 'ASC'
+                }
+            }
+            indexes[r.Key_name].columns.push(r.Column_name)
+        })
+        return Object.values(indexes)
+    }
+
+    async createIndex(index: DomainIndex): Promise<DomainIndex> {
+        const unique = index.isUnique ? 'UNIQUE' : ''
+        //TODO: support caseSensitive? 
+        await this.query(`CREATE ${unique} INDEX ${escapeId(index.name)} ON ${escapeTable(index.collectionName)} (${index.columns.map(escapeId).join(', ')})`)
+                  .catch( translateErrorCodes )
+        return index
+    }
+
+    async removeIndex(index: DomainIndex): Promise<void> {
+        await this.query(`DROP INDEX ${escapeId(index.name)} ON ${escapeTable(index.collectionName)}`)
+                  .catch( translateErrorCodes )
+    }
+}
