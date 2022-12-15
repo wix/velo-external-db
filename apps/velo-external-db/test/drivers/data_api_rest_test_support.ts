13208bdfba13bb6b3390eb7de5cd9a137beb1b13
Ido Kahlon <82806105+Idokah@users.noreply.github.com>
2022-12-15 10:42:47 +0200

    refactor app data e2e (#372)
    * refactor app_data_e2e
    
    * .

--- apps/velo-external-db/test/drivers/data_api_rest_test_support.ts
+++ apps/velo-external-db/test/drivers/data_api_rest_test_support.ts
@@ -1,9 +1,70 @@
+import axios from 'axios'
 import { Item } from '@wix-velo/velo-external-db-types'
+import { dataSpi } from '@wix-velo/velo-external-db-core'
+import { streamToArray } from '@wix-velo/test-commons'
 
-const axios = require('axios').create({
+const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080'
 })
 
-export const givenItems = async(items: Item[], collectionName: string, auth: any) => await axios.post('/data/insert/bulk', { collectionName: collectionName, items: items }, auth)
+export const insertRequest = (collectionName: string, items: Item[], overwriteExisting: boolean): dataSpi.InsertRequest => ({
+    collectionId: collectionName,
+    items: items,
+    overwriteExisting,
+    options: {
+        consistentRead: false,
+        appOptions: {},
+    }
+})
 
-export const expectAllDataIn = async(collectionName: string, auth: any) => (await axios.post('/data/find', { collectionName: collectionName, filter: '', sort: '', skip: 0, limit: 25 }, auth)).data
+export const updateRequest = (collectionName: string, items: Item[]): dataSpi.UpdateRequest => ({
+    collectionId: collectionName,
+    items: items,
+    options: {
+        consistentRead: false,
+        appOptions: {},
+    }
+})
+
+export const countRequest = (collectionName: string): dataSpi.CountRequest => ({
+    collectionId: collectionName,
+    filter: '',
+    options: {
+        consistentRead: false,
+        appOptions: {},
+    },
+})
+
+export const queryRequest = (collectionName: string, sort: dataSpi.Sorting[], fields: string[], filter?: dataSpi.Filter): dataSpi.QueryRequest => ({
+    collectionId: collectionName,
+    query: {
+        filter: filter ?? '',
+        sort: sort,
+        fields: fields,
+        fieldsets: undefined,
+        paging: {
+            limit: 25,
+            offset: 0,
+        },
+        cursorPaging: null
+    },
+    includeReferencedItems: [],
+    options: {
+        consistentRead: false,
+        appOptions: {},
+    },
+    omitTotalCount: false
+})
+
+
+export const queryCollectionAsArray = (collectionName: string, sort: dataSpi.Sorting[], fields: string[], auth: any, filter?: dataSpi.Filter) =>
+    axiosInstance.post('/data/query',
+        queryRequest(collectionName, sort, fields, filter), { responseType: 'stream', transformRequest: auth.transformRequest })
+        .then(response => streamToArray(response.data))
+
+
+export const pagingMetadata = (total: number, count: number): dataSpi.QueryResponsePart => ({ pagingMetadata: { count: count, offset: 0, total: total, tooManyToCount: false } })
+
+
+export const givenItems = async(items: Item[], collectionName: string, auth: any) =>
+    await axiosInstance.post('/data/insert', insertRequest(collectionName, items, false), { responseType: 'stream', transformRequest: auth.transformRequest })
