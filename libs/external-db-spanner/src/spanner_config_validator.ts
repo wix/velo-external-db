344cee502c1c53280194c864e20ee9351b125606
Ido Kahlon <idokk96@gmail.com>
2023-01-02 11:29:36 +0200

    adding secretKey to avoid conflicts

--- libs/external-db-spanner/src/spanner_config_validator.ts
+++ libs/external-db-spanner/src/spanner_config_validator.ts
@@ -5,7 +5,7 @@ export class ConfigValidator implements IConfigValidator {
     requiredKeys: string[]
     config: any
     constructor(config: any) {
-        this.requiredKeys = ['projectId', 'instanceId', 'databaseId']
+        this.requiredKeys = ['projectId', 'instanceId', 'databaseId', 'secretKey']
         this.config = config
     }
 
