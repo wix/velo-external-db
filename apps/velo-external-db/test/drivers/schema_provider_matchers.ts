0906c904d9258890e09c8dbef43095412a9a5c55
Max Polsky <maxim.polski@gmail.com>
2023-01-24 14:57:29 +0200

    Collection and field capabilities tests refactor  (#396)
    * refactor: extract ColumnsCapabilities to capabilities file
    
    * test: accurate matchers
    
    * refactor: extracted some types to a dedicated types file
    
    * refactor: some refactors based on code review

--- apps/velo-external-db/test/drivers/schema_provider_matchers.ts
+++ apps/velo-external-db/test/drivers/schema_provider_matchers.ts
@@ -3,7 +3,12 @@ import { ResponseField } from '@wix-velo/velo-external-db-types'
 
 export const hasSameSchemaFieldsLike = (fields: ResponseField[]) => expect.arrayContaining(fields.map((f) => expect.objectContaining( f )))
 
-export const toContainDefaultFields = () => hasSameSchemaFieldsLike(SystemFields.map(f => ({ field: f.name, type: f.type })))
+export const toContainDefaultFields = (columnsCapabilities:any) => hasSameSchemaFieldsLike(SystemFields.map(f => ({
+    field: f.name, 
+    type: f.type,
+    capabilities: columnsCapabilities[f.type]
+})))
+
 
 export const collectionToContainFields = (collectionName: string, fields: ResponseField[], capabilities: any) => ({
     id: collectionName,
