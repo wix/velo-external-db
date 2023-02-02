59305f8280308de1d4075116e36e9331953ebc69
michaelir <michaelir@wix.com>
2023-02-02 12:12:34 +0200

    removed redundant code

--- libs/external-db-postgres/src/postgres_utils.ts
+++ libs/external-db-postgres/src/postgres_utils.ts
@@ -1,8 +1,5 @@
 
 // Ported from PostgreSQL 9.2.4 source code in src/interfaces/libpq/fe-exec.c
-import { ColumnCapabilities } from '@wix-velo/velo-external-db-types'
-import { AdapterOperators } from '@wix-velo/velo-external-db-commons'
-const { eq, ne, string_contains, string_begins, string_ends, gt, gte, lt, lte, include } = AdapterOperators
 
 export const escapeIdentifier = (str: string) => str === '*' ? '*' : `"${(str || '').replace(/"/g, '""')}"`
 
@@ -22,43 +19,4 @@ export const prepareStatementVariablesForBulkInsert = (rowsCount: number, column
         segments.push('(' + segment.join(',') + ')')
     }
     return segments.join(',')
-}
-
-export const columnCapabilitiesFor = (columnType: string): ColumnCapabilities => {
-    switch (columnType) {
-        case 'text':
-        case 'url':
-            return {
-                sortable: true,
-                columnQueryOperators: [eq, ne, string_contains, string_begins, string_ends, include, gt, gte, lt, lte]
-            }
-        case 'number':
-            return {
-                sortable: true,
-                columnQueryOperators: [eq, ne, gt, gte, lt, lte, include]
-            }
-        case 'boolean':
-            return {
-                sortable: true,
-                columnQueryOperators: [eq]
-            }
-        case 'image':
-            return {
-                sortable: false,
-                columnQueryOperators: []
-            }
-        case 'object':
-            return {
-                sortable: true,
-                columnQueryOperators: [eq, ne]
-            }
-        case 'datetime':
-            return {
-                sortable: true,
-                columnQueryOperators: [eq, ne, gt, gte, lt, lte]
-            }
-
-        default:
-            throw new Error(`${columnType} - Unsupported field type`)
-    }
 }
