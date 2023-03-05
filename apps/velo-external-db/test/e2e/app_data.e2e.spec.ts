7a0950ec929f11a993d358cd9ec08ec59d9788be
Max Polsky <maxim.polski@gmail.com>
2023-03-05 16:59:20 +0200

    refactor: returned needed ops to the test

--- apps/velo-external-db/test/e2e/app_data.e2e.spec.ts
+++ apps/velo-external-db/test/e2e/app_data.e2e.spec.ts
@@ -11,7 +11,7 @@ import * as matchers from '../drivers/schema_api_rest_matchers'
 import * as data from '../drivers/data_api_rest_test_support'
 import * as authorization from '../drivers/authorization_test_support'
 import { initApp, teardownApp, dbTeardown, setupDb, currentDbImplementationName, supportedOperations } from '../resources/e2e_resources'
-const { UpdateImmediately, DeleteImmediately, Truncate, Aggregate, FindWithSort, Projection, FilterByEveryField, QueryNestedFields, PrimaryKey } = SchemaOperations
+const { UpdateImmediately, DeleteImmediately, Truncate, Aggregate, FindWithSort, Projection, FilterByEveryField, QueryNestedFields, PrimaryKey, NonAtomicBulkInsert } = SchemaOperations
 
 const chance = Chance()
 
@@ -98,7 +98,7 @@ describe(`Velo External DB Data REST API: ${currentDbImplementationName()}`,  ()
         )
     })
 
-    testIfSupportedOperationsIncludes(supportedOperations, [PrimaryKey])('insert api should throw 409 error if item already exists and continue inserting the rest', async() => {
+    testIfSupportedOperationsIncludes(supportedOperations, [NonAtomicBulkInsert, PrimaryKey])('insert api should throw 409 error if item already exists and continue inserting the rest', async() => {
         await schema.givenCollection(ctx.collectionName, [ctx.column], authOwner)
         await data.givenItems([ ctx.items[1] ], ctx.collectionName, authAdmin)
 
