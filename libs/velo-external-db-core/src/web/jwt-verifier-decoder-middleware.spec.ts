a9bdd5be2e065a4fdf527c8a01e97392df399746
Ido Kahlon <idokk96@gmail.com>
2023-09-21 15:50:37 +0300

    jwt verifier, decoder middleware

---
+++ libs/velo-external-db-core/src/web/jwt-verifier-decoder-middleware.spec.ts
@@ -0,0 +1,62 @@
+import * as Chance from 'chance'
+import { Request, Response } from 'express'
+import { Uninitialized, gen as genCommon } from '@wix-velo/test-commons'
+import { JWTVerifierDecoderMiddleware } from './jwt-verifier-decoder-middleware'
+import * as driver from './jwt-verifier-test-support'
+import { errors } from '@wix-velo/velo-external-db-commons'
+
+const { UnauthorizedError } = errors
+const chance = Chance()
+
+describe('Jwt Verifier Decoder Middleware', () => {
+    test('given valid token, should decode and override request body', async() => {
+        driver.givenValidToken(ctx.token, ctx.decodedToken)
+        env.JwtVerifierDecoderMiddleware.verifyAndDecodeMiddleware()(ctx.req, ctx.res, ctx.next)
+        expect(ctx.req.body).toEqual(ctx.decodedToken)
+    })
+
+    test('given invalid token, should throw error', async() => {
+        driver.givenInvalidToken(ctx.token, ctx.errorMsgFromVerifier)
+        env.JwtVerifierDecoderMiddleware.verifyAndDecodeMiddleware()(ctx.req, ctx.res, ctx.next)
+        expect(ctx.next).toHaveBeenCalledWith(new UnauthorizedError(ctx.errorMsgFromVerifier))
+    })
+    
+    interface Env {
+        JwtVerifierDecoderMiddleware: JWTVerifierDecoderMiddleware
+    }
+    const env: Env = {
+        JwtVerifierDecoderMiddleware: Uninitialized,
+    }
+
+    interface Ctx {
+        token: string,
+        req: Request
+        res: Response
+        next: jest.Mock
+        decodedToken: Record<string, unknown>
+        errorMsgFromVerifier: string
+    }
+
+    const ctx: Ctx = {
+        token: Uninitialized,
+        req: Uninitialized,
+        res: Uninitialized,
+        next: Uninitialized,
+        decodedToken: Uninitialized,
+        errorMsgFromVerifier: Uninitialized,
+    }
+
+    beforeEach(() => {
+        ctx.token = chance.string()
+        ctx.req = {
+            ...genCommon.randomObject(),
+            body: ctx.token,
+        } as Request
+        ctx.res = genCommon.randomObject() as Response
+        ctx.next = jest.fn()
+        ctx.decodedToken = genCommon.randomObject()
+        ctx.errorMsgFromVerifier = chance.string()
+
+        env.JwtVerifierDecoderMiddleware = new JWTVerifierDecoderMiddleware(driver.jwtVerifier)
+    })
+})
