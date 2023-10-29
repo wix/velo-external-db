673920b90c6469a4657cb58cb46637714723b8fa
Max Polsky <maxim.polski@gmail.com>
2023-10-29 12:49:18 +0200

    test: enable error logs

--- libs/velo-external-db-core/src/web/error-middleware.ts
+++ libs/velo-external-db-core/src/web/error-middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from 'express'
 import { domainToSpiErrorTranslator } from './domain-to-spi-error-translator'
 
 export const errorMiddleware = (err: any, _req: any, res: Response, _next?: NextFunction) => {
-  if (process.env['NODE_ENV'] !== 'test') {
+  if (process.env['NODE_ENV'] === 'test') {
     console.error(err)
   }
 
