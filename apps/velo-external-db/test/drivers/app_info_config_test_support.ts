4ebac36336d36e2fb0f6d3153653be995fbc3247
Max Polsky <maxim.polski@gmail.com>
2023-10-31 14:58:34 +0200

    test: fix for tests in node20

--- apps/velo-external-db/test/drivers/app_info_config_test_support.ts
+++ apps/velo-external-db/test/drivers/app_info_config_test_support.ts
@@ -4,4 +4,7 @@ export const givenHideAppInfoEnvIsTrue = async() => {
     await dbTeardown()
     process.env.HIDE_APP_INFO = 'true'
     await initApp()
+
+    // Fix for tests running in Node 20.
+    await new Promise(resolve => setTimeout(resolve, 5))
 }
