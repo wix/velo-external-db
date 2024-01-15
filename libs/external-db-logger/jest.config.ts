e1826cd5a2eb5bbd782e0886f287390bf00a823d
Max Polsky <maxim.polski@gmail.com>
2024-01-15 11:17:47 +0200

    init of external-db-logger

---
+++ libs/external-db-logger/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'external-db-logger',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/external-db-logger',
+  verbose: true,
+};
