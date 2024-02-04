b0860bfe6622f9385110c6c20758d25d896d3e31
Ido Kahlon <idokk96@gmail.com>
2024-02-04 15:39:29 +0200

    don't support in spanner

--- libs/external-db-spanner/src/supported_operations.ts
+++ libs/external-db-spanner/src/supported_operations.ts
@@ -1,6 +1,6 @@
 import { AllSchemaOperations } from '@wix-velo/velo-external-db-commons'
 import { SchemaOperations } from '@wix-velo/velo-external-db-types'
 //change column types - https://cloud.google.com/spanner/docs/schema-updates#supported_schema_updates
-const notSupportedOperations = [SchemaOperations.ChangeColumnType, SchemaOperations.NonAtomicBulkInsert]
+const notSupportedOperations = [SchemaOperations.ChangeColumnType, SchemaOperations.NonAtomicBulkInsert, SchemaOperations.FindObject]
 
 export const supportedOperations = AllSchemaOperations.filter(op => !notSupportedOperations.includes(op))
